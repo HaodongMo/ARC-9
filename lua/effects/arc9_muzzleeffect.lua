071a228418d79bb1f56c85f33e27f36e30fb4fc9
Haodong Mo <mohaodong@gmail.com>
2022-01-25 15:47:14 +1000

    Update arc9_muzzleeffect.lua

--- lua/effects/arc9_muzzleeffect.lua
+++ lua/effects/arc9_muzzleeffect.lua
@@ -20,12 +20,30 @@ function EFFECT:Init(data)
         parent = LocalPlayer():GetViewModel()
     end
 
+    local pa = parent:GetAttachment(att)
+    local pos = pa.Pos
+
     parent = wpn:GetMuzzleDevice(wm)
 
     -- if !IsValid(parent) then return end
 
     if muzzle then
         ParticleEffectAttach(muzzle, PATTACH_POINT_FOLLOW, parent, att)
+    end
+
+    if !wpn:GetProcessedValue("Silencer") and !wpn:GetProcessedValue("NoFlash") then
+        local light = DynamicLight(self:EntIndex())
+        local clr = Color(244, 209, 66)
+        if (light) then
+            light.Pos = pos
+            light.r = clr.r
+            light.g = clr.g
+            light.b = clr.b
+            light.Brightness = 2
+            light.Decay = 2500
+            light.Size = wpn:GetOwner() == LocalPlayer() and 256 or 128
+            light.DieTime = CurTime() + 0.1
+        end
     end
 end
 
