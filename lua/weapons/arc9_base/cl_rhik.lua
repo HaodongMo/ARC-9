735d473de77ec74eaa757d590367a10ca972cb8e
Haodong Mo <mohaodong@gmail.com>
2022-02-18 14:36:44 +1000

    Update cl_rhik.lua

--- lua/weapons/arc9_base/cl_rhik.lua
+++ lua/weapons/arc9_base/cl_rhik.lua
@@ -157,119 +157,121 @@ function SWEP:DoRHIK()
         end
     end
 
-    local rupperarm, rforearm, rulna, rwrist, rhand = vm:LookupBone("ValveBiped.Bip01_R_UpperArm"), vm:LookupBone("ValveBiped.Bip01_R_Forearm"), vm:LookupBone("ValveBiped.Bip01_R_Ulna"), vm:LookupBone("ValveBiped.Bip01_R_Wrist"), vm:LookupBone("ValveBiped.Bip01_R_Hand")
-    local lupperarm, lforearm, lulna, lwrist, lhand = vm:LookupBone("ValveBiped.Bip01_L_UpperArm"), vm:LookupBone("ValveBiped.Bip01_L_Forearm"), vm:LookupBone("ValveBiped.Bip01_L_Ulna"), vm:LookupBone("ValveBiped.Bip01_L_Wrist"), vm:LookupBone("ValveBiped.Bip01_L_Hand")
-
-    local rupperarm_matrix, rhand_matrix = vm:GetBoneMatrix(rupperarm), vm:GetBoneMatrix(rhand)
-    local lupperarm_matrix, lhand_matrix = vm:GetBoneMatrix(lupperarm), vm:GetBoneMatrix(lhand)
-    local rforearm_matrix = vm:GetBoneMatrix(rforearm)
-    local lforearm_matrix = vm:GetBoneMatrix(lforearm)
-    local rarm_start, rhand_end = rupperarm_matrix:GetTranslation(), rhand_matrix:GetTranslation()
-    local larm_start, lhand_end = lupperarm_matrix:GetTranslation(), lhand_matrix:GetTranslation()
-
-    local rupperarm_length, rarm_length = vm:BoneLength(rupperarm), vm:BoneLength(rforearm)
-    if rupperarm_length > 15 or rarm_length > 15 or rupperarm_length < 5 or rarm_length < 5 then
-        rupperarm_length = 8
-        rarm_length = 8
-    end
-    local lupperarm_length, larm_length = vm:BoneLength(lupperarm), vm:BoneLength(lforearm)
-    if lupperarm_length > 15 or larm_length > 15 or lupperarm_length < 5 or larm_length < 5 then
-        lupperarm_length = 8
-        larm_length = 8
-    end
-
-    -- lupperarm_length = lupperarm_length + 2
-    -- larm_length = larm_length + 2
-
-    rupperarm_matrix, rhand_matrix = vm:GetBoneMatrix(rupperarm), vm:GetBoneMatrix(rhand)
-    lupperarm_matrix, lhand_matrix = vm:GetBoneMatrix(lupperarm), vm:GetBoneMatrix(lhand)
-
-    rarm_start = rupperarm_matrix:GetTranslation()
-    larm_start = lupperarm_matrix:GetTranslation()
-
-    local rupperarm_position, rforearm_position = self:Solve2PartIK(rarm_start, rhand_end, rupperarm_length , rarm_length, -35)
-    local lupperarm_position, lforearm_position = self:Solve2PartIK(larm_start, lhand_end, lupperarm_length , larm_length, 35)
-
-    debugoverlay.Line(rarm_start, rupperarm_position, 0.1, Color( 255, 255, 255 ), true)
-    debugoverlay.Line(rforearm_position, rupperarm_position, 0.1, Color( 255, 255, 255 ), true)
-    debugoverlay.Line(rforearm_position, rhand_end, 0.1, Color( 255, 255, 255 ), true)
-
-    debugoverlay.Line(larm_start, lupperarm_position, 0.1, Color( 255, 255, 255 ), true)
-    debugoverlay.Line(lforearm_position, lupperarm_position, 0.1, Color( 255, 255, 255 ), true)
-    debugoverlay.Line(lforearm_position, lhand_end, 0.1, Color( 255, 255, 255 ), true)
-
-    -- rupperarm_matrix:SetTranslation(rupperarm_position)
-    -- brought to you by: https://rubberduckdebugging.com/
-    -- get one today!
-    -- right
-
     local enable_ik = false
 
-    if self:GetValue("RHIK") and enable_ik then
-        local rupperarm_dir = (rupperarm_position - rupperarm_matrix:GetTranslation())
-        rupperarm_matrix:SetAngles(rupperarm_dir:Angle())
-        local rupperarm_norm = (rupperarm_position-rarm_start)
-        rupperarm_norm:Normalize()
-        rupperarm_matrix:SetTranslation(rarm_start - rupperarm_norm * 4)
+    if enable_ik then
+        local rupperarm, rforearm, rulna, rwrist, rhand = vm:LookupBone("ValveBiped.Bip01_R_UpperArm"), vm:LookupBone("ValveBiped.Bip01_R_Forearm"), vm:LookupBone("ValveBiped.Bip01_R_Ulna"), vm:LookupBone("ValveBiped.Bip01_R_Wrist"), vm:LookupBone("ValveBiped.Bip01_R_Hand")
+        local lupperarm, lforearm, lulna, lwrist, lhand = vm:LookupBone("ValveBiped.Bip01_L_UpperArm"), vm:LookupBone("ValveBiped.Bip01_L_Forearm"), vm:LookupBone("ValveBiped.Bip01_L_Ulna"), vm:LookupBone("ValveBiped.Bip01_L_Wrist"), vm:LookupBone("ValveBiped.Bip01_L_Hand")
 
-        local rforearm_norm = (rforearm_position-rforearm_matrix:GetTranslation())
-        rforearm_norm:Normalize()
-        rforearm_matrix:SetTranslation(rupperarm_position + rforearm_norm * 2)
-        local rforearm_dir = rhand_end - rupperarm_position
-        rforearm_matrix:SetAngles(rforearm_dir:Angle())
+        local rupperarm_matrix, rhand_matrix = vm:GetBoneMatrix(rupperarm), vm:GetBoneMatrix(rhand)
+        local lupperarm_matrix, lhand_matrix = vm:GetBoneMatrix(lupperarm), vm:GetBoneMatrix(lhand)
+        local rforearm_matrix = vm:GetBoneMatrix(rforearm)
+        local lforearm_matrix = vm:GetBoneMatrix(lforearm)
+        local rarm_start, rhand_end = rupperarm_matrix:GetTranslation(), rhand_matrix:GetTranslation()
+        local larm_start, lhand_end = lupperarm_matrix:GetTranslation(), lhand_matrix:GetTranslation()
 
-        vm:SetBoneMatrix(rupperarm, rupperarm_matrix)
-        vm:SetBoneMatrix(rforearm, rforearm_matrix)
-
-        if rulna and rwrist then
-            local rwrist_matrix = vm:GetBoneMatrix(rwrist)
-            local rulna_matrix = vm:GetBoneMatrix(rulna)
-
-            local rwrist_angle = (rwrist_matrix:GetTranslation() - rupperarm_position):Angle()
-            rwrist_matrix:SetAngles(rwrist_angle)
-            vm:SetBoneMatrix(rwrist, rwrist_matrix)
-
-            -- rwrist_angle.r = rwrist_matrix:GetAngles().r + 90
-
-            rulna_matrix:SetTranslation(rwrist_matrix:GetTranslation() - (rwrist_angle:Forward() * 4))
-            rulna_matrix:SetAngles(rwrist_angle)
-            vm:SetBoneMatrix(rulna, rulna_matrix)
+        local rupperarm_length, rarm_length = vm:BoneLength(rupperarm), vm:BoneLength(rforearm)
+        if rupperarm_length > 15 or rarm_length > 15 or rupperarm_length < 5 or rarm_length < 5 then
+            rupperarm_length = 8
+            rarm_length = 8
         end
-    end
+        local lupperarm_length, larm_length = vm:BoneLength(lupperarm), vm:BoneLength(lforearm)
+        if lupperarm_length > 15 or larm_length > 15 or lupperarm_length < 5 or larm_length < 5 then
+            lupperarm_length = 8
+            larm_length = 8
+        end
 
-    -- brought to you by: https://rubberduckdebugging.com/
-    -- get one today!
-    -- left
-    if self:GetValue("LHIK") and enable_ik then
-        local lupperarm_dir = (lupperarm_position - lupperarm_matrix:GetTranslation())
-        lupperarm_matrix:SetAngles(lupperarm_dir:Angle())
-        local lupperarm_norm = (lupperarm_position-larm_start)
-        lupperarm_norm:Normalize()
-        lupperarm_matrix:SetTranslation(larm_start - (lupperarm_norm * 0))
+        -- lupperarm_length = lupperarm_length + 2
+        -- larm_length = larm_length + 2
 
-        local lforearm_norm = (lforearm_position-lforearm_matrix:GetTranslation())
-        lforearm_norm:Normalize()
-        lforearm_matrix:SetTranslation(lupperarm_position + (lforearm_norm * 0))
-        local lforearm_dir = lhand_end - lupperarm_position
-        lforearm_matrix:SetAngles(lforearm_dir:Angle())
+        rupperarm_matrix, rhand_matrix = vm:GetBoneMatrix(rupperarm), vm:GetBoneMatrix(rhand)
+        lupperarm_matrix, lhand_matrix = vm:GetBoneMatrix(lupperarm), vm:GetBoneMatrix(lhand)
 
-        vm:SetBoneMatrix(lupperarm, lupperarm_matrix)
-        vm:SetBoneMatrix(lforearm, lforearm_matrix)
+        rarm_start = rupperarm_matrix:GetTranslation()
+        larm_start = lupperarm_matrix:GetTranslation()
 
-        if lulna and lwrist then
-            local lwrist_matrix = vm:GetBoneMatrix(lwrist)
-            local lulna_matrix = vm:GetBoneMatrix(lulna)
+        local rupperarm_position, rforearm_position = self:Solve2PartIK(rarm_start, rhand_end, rupperarm_length , rarm_length, -35)
+        local lupperarm_position, lforearm_position = self:Solve2PartIK(larm_start, lhand_end, lupperarm_length , larm_length, 35)
 
-            local lwrist_angle = (lwrist_matrix:GetTranslation() - lupperarm_position):Angle()
-            lwrist_matrix:SetTranslation(lwrist_matrix:GetTranslation())
-            lwrist_matrix:SetAngles(lwrist_angle)
-            vm:SetBoneMatrix(lwrist, lwrist_matrix)
+        debugoverlay.Line(rarm_start, rupperarm_position, 0.1, Color( 255, 255, 255 ), true)
+        debugoverlay.Line(rforearm_position, rupperarm_position, 0.1, Color( 255, 255, 255 ), true)
+        debugoverlay.Line(rforearm_position, rhand_end, 0.1, Color( 255, 255, 255 ), true)
 
-            -- lwrist_angle.r = lwrist_matrix:GetAngles().r + 90
+        debugoverlay.Line(larm_start, lupperarm_position, 0.1, Color( 255, 255, 255 ), true)
+        debugoverlay.Line(lforearm_position, lupperarm_position, 0.1, Color( 255, 255, 255 ), true)
+        debugoverlay.Line(lforearm_position, lhand_end, 0.1, Color( 255, 255, 255 ), true)
 
-            lulna_matrix:SetTranslation(lwrist_matrix:GetTranslation() - (lwrist_angle:Forward() * 4))
-            lulna_matrix:SetAngles(lwrist_angle)
-            vm:SetBoneMatrix(lulna, lulna_matrix)
+        -- rupperarm_matrix:SetTranslation(rupperarm_position)
+        -- brought to you by: https://rubberduckdebugging.com/
+        -- get one today!
+        -- right
+
+        if self:GetValue("RHIK") and enable_ik then
+            local rupperarm_dir = (rupperarm_position - rupperarm_matrix:GetTranslation())
+            rupperarm_matrix:SetAngles(rupperarm_dir:Angle())
+            local rupperarm_norm = (rupperarm_position-rarm_start)
+            rupperarm_norm:Normalize()
+            rupperarm_matrix:SetTranslation(rarm_start - rupperarm_norm * 4)
+
+            local rforearm_norm = (rforearm_position-rforearm_matrix:GetTranslation())
+            rforearm_norm:Normalize()
+            rforearm_matrix:SetTranslation(rupperarm_position + rforearm_norm * 2)
+            local rforearm_dir = rhand_end - rupperarm_position
+            rforearm_matrix:SetAngles(rforearm_dir:Angle())
+
+            vm:SetBoneMatrix(rupperarm, rupperarm_matrix)
+            vm:SetBoneMatrix(rforearm, rforearm_matrix)
+
+            if rulna and rwrist then
+                local rwrist_matrix = vm:GetBoneMatrix(rwrist)
+                local rulna_matrix = vm:GetBoneMatrix(rulna)
+
+                local rwrist_angle = (rwrist_matrix:GetTranslation() - rupperarm_position):Angle()
+                rwrist_matrix:SetAngles(rwrist_angle)
+                vm:SetBoneMatrix(rwrist, rwrist_matrix)
+
+                -- rwrist_angle.r = rwrist_matrix:GetAngles().r + 90
+
+                rulna_matrix:SetTranslation(rwrist_matrix:GetTranslation() - (rwrist_angle:Forward() * 4))
+                rulna_matrix:SetAngles(rwrist_angle)
+                vm:SetBoneMatrix(rulna, rulna_matrix)
+            end
+        end
+
+        -- brought to you by: https://rubberduckdebugging.com/
+        -- get one today!
+        -- left
+        if self:GetValue("LHIK") and enable_ik then
+            local lupperarm_dir = (lupperarm_position - lupperarm_matrix:GetTranslation())
+            lupperarm_matrix:SetAngles(lupperarm_dir:Angle())
+            local lupperarm_norm = (lupperarm_position-larm_start)
+            lupperarm_norm:Normalize()
+            lupperarm_matrix:SetTranslation(larm_start - (lupperarm_norm * 0))
+
+            local lforearm_norm = (lforearm_position-lforearm_matrix:GetTranslation())
+            lforearm_norm:Normalize()
+            lforearm_matrix:SetTranslation(lupperarm_position + (lforearm_norm * 0))
+            local lforearm_dir = lhand_end - lupperarm_position
+            lforearm_matrix:SetAngles(lforearm_dir:Angle())
+
+            vm:SetBoneMatrix(lupperarm, lupperarm_matrix)
+            vm:SetBoneMatrix(lforearm, lforearm_matrix)
+
+            if lulna and lwrist then
+                local lwrist_matrix = vm:GetBoneMatrix(lwrist)
+                local lulna_matrix = vm:GetBoneMatrix(lulna)
+
+                local lwrist_angle = (lwrist_matrix:GetTranslation() - lupperarm_position):Angle()
+                lwrist_matrix:SetTranslation(lwrist_matrix:GetTranslation())
+                lwrist_matrix:SetAngles(lwrist_angle)
+                vm:SetBoneMatrix(lwrist, lwrist_matrix)
+
+                -- lwrist_angle.r = lwrist_matrix:GetAngles().r + 90
+
+                lulna_matrix:SetTranslation(lwrist_matrix:GetTranslation() - (lwrist_angle:Forward() * 4))
+                lulna_matrix:SetAngles(lwrist_angle)
+                vm:SetBoneMatrix(lulna, lulna_matrix)
+            end
         end
     end
 
