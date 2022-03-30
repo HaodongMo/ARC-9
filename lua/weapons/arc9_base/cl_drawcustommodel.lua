314b542758c2d34c54a90f98b8ec01347be7e68c
Haodong Mo <mohaodong@gmail.com>
2022-03-30 19:16:54 +1100

    ugh

--- lua/weapons/arc9_base/cl_drawcustommodel.lua
+++ lua/weapons/arc9_base/cl_drawcustommodel.lua
@@ -70,21 +70,17 @@ function SWEP:DrawCustomModel(wm, custompos, customang)
             --     self:DoHolosight(model, atttbl)
             -- end
 
-            if !ARC9.PresetCam then
-                if atttbl.DrawFunc then
-                    atttbl.DrawFunc(self, model, wm)
-                end
+            if atttbl.DrawFunc then
+                atttbl.DrawFunc(self, model, wm)
+            end
 
+            if !ARC9.PresetCam then
                 if !wm and atttbl.RTScope then
                     local active = slottbl == self:GetActiveSightSlotTable()
                     self:DoRTScope(model, atttbl, active)
                 elseif wm and atttbl.RTScope then
                     model:SetSubMaterial(atttbl.RTScopeSubmatIndex, "vgui/black")
                 end
-            end
-
-            if model.DrawFunc then
-                model.DrawFunc(self, model)
             end
 
             if !model.NoDraw then
