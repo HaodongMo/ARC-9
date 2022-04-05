7c204fcd52d01d5a99f8fc43a7ef58e5c8e58708
Haodong Mo <mohaodong@gmail.com>
2022-04-05 14:17:28 +1000

    Fixed PP

--- lua/arc9/client/cl_pp.lua
+++ lua/arc9/client/cl_pp.lua
@@ -1,4 +1,4 @@
-hook.Add("RenderScreenspaceEffects", "ARC9_SSE", function()
+hook.Add("RenderScreenspaceEffects", "ARC9_SSE_PP", function()
     local wpn = LocalPlayer():GetActiveWeapon()
 
     if !IsValid(wpn) then return end
