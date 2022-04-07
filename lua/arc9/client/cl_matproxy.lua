77ffcbe4e1d89b04212753f5aa41d765bc49f12e
Haodong Mo <mohaodong@gmail.com>
2022-04-07 17:11:09 +1000

    more changes

--- lua/arc9/client/cl_matproxy.lua
+++ lua/arc9/client/cl_matproxy.lua
@@ -4,17 +4,27 @@ matproxy.Add({
         -- Store the name of the variable we want to set
         self.DetailResult = values.camotexture
         self.ScaleResult = values.camoscale
-        -- self.BlendResult = values.camoBlendFactor
-        -- self.BlendModeResult = values.camoBlendMode
+
+        if self.DetailResult then
+            self.DefaultTexture = mat:GetTexture(self.DetailResult)
+        end
+
+        if self.ScaleResult then
+            self.DefaultScale = mat:GetFloat(self.ScaleResult)
+        end
     end,
     bind = function( self, mat, ent )
         if self.DetailResult and ent.CustomCamoTexture then
             -- mat:SetString(self.DetailResult, self.CustomCamoTexture)
             mat:SetTexture(self.DetailResult, ent.CustomCamoTexture)
+        elseif self.DetailResult then
+            mat:SetTexture(self.DetailResult, self.DefaultTexture)
         end
 
         if self.ScaleResult and ent.CustomCamoScale then
             mat:SetFloat(self.ScaleResult, ent.CustomCamoScale)
+        elseif self.ScaleResult then
+            mat:SetFloat(self.ScaleResult, self.DefaultScale)
         end
 
     end
