1583a99a961b1a7d08d7de084e088be03ad30894
1darky <95997959+1darky@users.noreply.github.com>
2022-05-11 14:57:14 +0700

    AdditionalSightDistance

--- lua/weapons/arc9_base/cl_autosolve.lua
+++ lua/weapons/arc9_base/cl_autosolve.lua
@@ -12,7 +12,7 @@ function SWEP:GenerateAutoSight(sight, slottbl)
 
     debugoverlay.Axis(pos, ang, 16, 1, true)
 
-    local s_pos = Vector(0, 0, 0)
+    local s_pos = Vector(0, self:GetProcessedValue("AdditionalSightDistance"), 0)
     local s_ang = Angle(0, 0, 0)
 
     local up, forward, right = s_ang:Up(), s_ang:Forward(), s_ang:Right()
