a53804e32135a8b26e20af17b03ba2f2a746cd04
1darky <95997959+dar-su@users.noreply.github.com>
2022-09-07 16:10:15 +0700

    different unbound key strng

--- lua/arc9/client/cl_bind.lua
+++ lua/arc9/client/cl_bind.lua
@@ -63,7 +63,7 @@ function ARC9.GetBindKey(bind)
     if CTRL then
         return bind
     elseif !key then
-        return bind .. " UNBOUND"
+        return "bind KEY "..  bind
     else
         return string.upper(key)
     end
