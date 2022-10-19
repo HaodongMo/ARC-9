e6c8acd62f95b4f16cef00fa5ac385810de95a60
1darky <95997959+dar-su@users.noreply.github.com>
2022-10-20 00:08:06 +0700

    shh

---
+++ Stuff for developers/Temp workfolder delete later/uc_charm_urbancharm_decay.lua
@@ -0,0 +1,23 @@
+att.PrintName = "Urban Charm - Decay"
+att.Description = "The icon of a Garry's Mod addon."
+
+att.Desc_Pros = {
+}
+att.Desc_Cons = {
+}
+att.AutoStats = true
+att.Slot = "charm"
+
+att.Free = true
+
+att.Model = "models/items/arc9/att_charmbase.mdl"
+
+att.DroppedModel = "models/Items/BoxSRounds.mdl"
+
+att.Charm = true
+att.CharmModel = "models/weapons/arccw/atts/uc_urbancharm.mdl"
+att.CharmAtt = "Charm"
+att.CharmScale = Vector(0.5, 0.5, 0.5)
+att.CharmOffset = Vector(0, -1.1, -0.2)
+att.CharmAngle = Angle(20, 0, 80)
+att.CharmSkin = 1
