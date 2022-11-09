a41e06443008b2a192867b2abe8df27c5884fa06
Haodong Mo <mohaodong@gmail.com>
2022-11-09 23:21:08 +1000

    Added nade base to base nades off of

---
+++ lua/weapons/arc9_base_nade.lua
@@ -0,0 +1,23 @@
+SWEP.Base = "arc9_base"
+SWEP.PrintName = "ARC9 Base Grenade"
+SWEP.Spawnable = false
+
+SWEP.BottomlessClip = true
+
+SWEP.ShootEnt = "arc9_proj_base"
+
+SWEP.Throwable = true -- Set to true to give this weapon throwing capabilities.
+SWEP.Tossable = true -- When grenade is enabled, right click will toss. Set to false to disable, allowing you to aim down sights.
+SWEP.ThrowAnimSpeed = 1
+
+SWEP.FuseTimer = -1 -- Length of time that the grenade will take to explode in your hands. -1 = Won't explode.
+
+SWEP.ThrowForceMin = 500 -- Minimum force that the grenade will be thrown with.
+SWEP.ThrowForceMax = 1000 -- Maximum force that the grenade will be thrown with.
+SWEP.TossForce = 250 -- Force that the grenade will be thrown with when right clicked.
+
+SWEP.ThrowChargeTime = 1 -- How long it takes to charge the grenade to its maximum throw force.
+
+SWEP.ThrowTumble = true -- Grenade tumbles when thrown.
+
+SWEP.Disposable = true
