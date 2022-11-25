8b3f305423091af579c78450e9c20db3b41afbbd
Haodong Mo <mohaodong@gmail.com>
2022-11-26 00:08:02 +1000

    More improvement to cheapscope aim

--- lua/weapons/arc9_base/cl_camera.lua
+++ lua/weapons/arc9_base/cl_camera.lua
@@ -17,6 +17,12 @@ function SWEP:CalcView(ply, pos, ang, fov)
         ang.r = ang.r + (math.sin(CurTime() * self:GetProcessedValue("RecoilKickDamping")) * rec)
     end
 
+    if self:IsScoping() and GetConVar("arc9_cheapscopes"):GetBool() then
+        local _, shootang = self:GetShootPos()
+
+        ang = LerpAngle(self:GetSightAmount(), ang, shootang)
+    end
+
     -- EFT like recoil
     if self.ViewRecoil then
         local ftmult = self:GetProcessedValue("RecoilDissipationRate") / 3
