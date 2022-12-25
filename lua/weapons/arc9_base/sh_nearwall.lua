58ecbafe5ea31e40881213b69d1cf8b0e3360d7b
Haodong Mo <mohaodong@gmail.com>
2022-12-25 22:27:30 +1000

    optimization of nearwall when 0 barrel length

--- lua/weapons/arc9_base/sh_nearwall.lua
+++ lua/weapons/arc9_base/sh_nearwall.lua
@@ -6,9 +6,13 @@ function SWEP:GetIsNearWall()
         return self.NearWallCached
     end
 
+    local length = self:GetProcessedValue("BarrelLength")
+
+    if length == 0 then return false end
+
     local tr = util.TraceLine({
         start = self:GetShootPos(),
-        endpos = self:GetShootPos() + self:GetShootDir():Forward() * self:GetProcessedValue("BarrelLength"),
+        endpos = self:GetShootPos() + self:GetShootDir():Forward() * length,
         filter = self:GetOwner(),
         mask = MASK_SHOT_HULL
     })
