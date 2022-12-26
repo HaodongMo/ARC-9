75c887bbbd0dc1113c4d7fdcde08f6fa54ebf937
Darsu <95997959+dar-su@users.noreply.github.com>
2022-12-26 15:04:53 +0700

    a9k + custom sticker

---
+++ lua/arc9/common/attachments/sticker_a9k.lua
@@ -0,0 +1,14 @@
+ATT.PrintName = [[ARCTICTHRON 9.000 WEAPON BASE]]
+ATT.CompactName = "A9K"
+ATT.Description = [[ARCTICTHRON 9.000 WEAPON BASE
+
+Sticker included in ARC9 Base.]]
+ATT.Icon = Material("stickers/a9k")
+
+ATT.SortOrder = 0
+ATT.Free = true
+
+ATT.Folder = "Stickers"
+ATT.Category = "stickers"
+
+ATT.StickerMaterial = "stickers/a9k"
---
+++ lua/arc9/common/attachments/sticker_spray.lua
@@ -0,0 +1,17 @@
+ATT.PrintName = [[Your spray here]]
+ATT.CompactName = "Custom"
+ATT.Description = [[write some description idk
+need write something about this sticker uses local material so everyone will see their own spray
+
+Sticker included in ARC9 Base.]]
+-- ATT.Icon = Material("decals/playerlogo01")
+ATT.Icon = Material("stickers/spray")
+
+ATT.SortOrder = 0
+ATT.Free = true
+
+ATT.Folder = "Stickers"
+ATT.Category = "stickers"
+
+-- ATT.StickerMaterial = "decals/playerlogo01"
+ATT.StickerMaterial = "stickers/spray"
