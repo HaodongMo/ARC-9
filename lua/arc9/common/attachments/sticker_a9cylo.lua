48720397ad198c85497b5b18d0db551134eab7ad
Darsu <95997959+dar-su@users.noreply.github.com>
2022-12-26 20:13:58 +0700

    cylo sticker

---
+++ lua/arc9/common/attachments/sticker_a9cylo.lua
@@ -0,0 +1,14 @@
+ATT.PrintName = [[ARC9 Concept logo by CyloWalker]]
+ATT.CompactName = "A9 Cylo"
+ATT.Description = [[One of the first concepts for ARC9 logo/icon by CyloWalker (he's cool!)
+
+Sticker included in ARC9 Base.]]
+ATT.Icon = Material("stickers/a9cylo")
+
+ATT.SortOrder = 0
+ATT.Free = true
+
+ATT.Folder = "Stickers"
+ATT.Category = "stickers"
+
+ATT.StickerMaterial = "stickers/a9cylo"
