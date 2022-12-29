d32b98b073cf00716b52c731dab5411c88f13d7e
Haodong Mo <mohaodong@gmail.com>
2022-12-29 17:26:18 +1000

    Refactor stickers to bulk atts and add alien lean

---
+++ lua/arc9/common/attachments_bulk/stickers_base.lua
@@ -0,0 +1,114 @@
+local ATT
+
+ATT = {}
+
+ATT.PrintName = [[ARC9 Logo (Low Vis)]]
+ATT.CompactName = "ARC9 LOWVIS"
+ATT.Description = [[Dark version of the ARC9 Logo.
+
+Sticker included in ARC9 Base.]]
+ATT.Icon = Material("stickers/arc9_lowvis")
+
+ATT.SortOrder = 0
+ATT.Free = true
+
+ATT.Category = "stickers"
+
+ATT.StickerMaterial = "stickers/arc9_lowvis"
+
+ARC9.LoadAttachment(ATT, "sticker_arc9")
+
+ATT = {}
+
+ATT.PrintName = [[ARC9 Logo]]
+ATT.CompactName = "ARC9"
+ATT.Description = [[Here's to you.
+
+Sticker included in ARC9 Base.]]
+ATT.Icon = Material("stickers/arc9")
+
+ATT.SortOrder = 0
+ATT.Free = true
+
+ATT.Category = "stickers"
+
+ATT.StickerMaterial = "stickers/arc9"
+
+ARC9.LoadAttachment(ATT, "sticker_arc9_lowvis")
+
+ATT = {}
+
+ATT.PrintName = [[ARCTICTHRON 9.000 WEAPON BASE]]
+ATT.CompactName = "A9K"
+ATT.Description = [[this is what "arc9" actually means
+
+Sticker included in ARC9 Base.]]
+ATT.Icon = Material("stickers/a9k")
+
+ATT.SortOrder = 0
+ATT.Free = true
+
+ATT.Category = "stickers"
+
+ATT.StickerMaterial = "stickers/a9k"
+
+
+ARC9.LoadAttachment(ATT, "sticker_a9k")
+
+ATT = {}
+
+ATT.PrintName = [[ARC9 Concept logo by CyloWalker]]
+ATT.CompactName = "A9 Cylo"
+ATT.Description = [[One of the first concepts for ARC9 logo/icon by CyloWalker (he's cool!)
+
+Sticker included in ARC9 Base.]]
+ATT.Icon = Material("stickers/a9cylo")
+
+ATT.SortOrder = 0
+ATT.Free = true
+
+ATT.Category = "stickers"
+
+ATT.StickerMaterial = "stickers/a9cylo"
+
+ARC9.LoadAttachment(ATT, "sticker_a9cylo")
+
+ATT = {}
+
+ATT.PrintName = [[Legalize Alien Lean]]
+ATT.CompactName = "Alien Lean"
+ATT.Description = [[Hop down to Agartha with the white boys for a cup of alien lean and... other things.
+Design by Opt1ca.
+
+Sticker included in ARC9 Base.]]
+ATT.Icon = Material("stickers/alien_lean")
+
+ATT.SortOrder = 0
+ATT.Free = true
+
+ATT.Category = "stickers"
+
+ATT.StickerMaterial = "stickers/alien_lean"
+
+ARC9.LoadAttachment(ATT, "sticker_alien_lean")
+
+
+ATT = {}
+
+ATT.PrintName = [[Your Spray Here]]
+ATT.CompactName = "Custom"
+ATT.Description = [[Sadly, you will only see your own spray, and not others'.
+
+Sticker included in ARC9 Base.]]
+-- ATT.Icon = Material("decals/playerlogo01")
+ATT.Icon = Material("stickers/spray")
+
+ATT.SortOrder = 0
+ATT.Free = true
+
+ATT.Category = "stickers"
+
+-- ATT.StickerMaterial = "decals/playerlogo01"
+ATT.StickerMaterial = "stickers/spray"
+
+ARC9.LoadAttachment(ATT, "sticker_spray")
