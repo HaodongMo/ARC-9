4f777f40fc2088de1714ffac2e98cc20e300dd7f
Aerosun <mister541thatguy@gmail.com>
2023-01-13 03:57:00 -0600

    Added various flat color camos to the base.
    Includes camouflage colors and ROYGBIV colors.

---
+++ lua/arc9/common/attachments_bulk/camos_base_flat_camo.lua
@@ -0,0 +1,125 @@
+local ATT = {}
+
+ATT.PrintName = "Olive Drab"
+ATT.CompactName = "OD"
+ATT.Icon = Material("models/weapons/arc9/camo/arc9_flatcamo/flat_od.png")
+ATT.Category = "universal_camo"
+ATT.Description = [[Standard Olive Drab camouflage.]]
+ATT.MenuCategory = "ARC-9 - Camos"
+ATT.CustomCamoTexture = "models/weapons/arc9/camo/arc9_flatcamo/vtf/flat_od"
+ATT.CustomCamoScale = 1
+ATT.Folder = "Base/Flat Color/Camouflage"
+
+ARC9.LoadAttachment(ATT, "camo_arc9_base_flat_od")
+
+ATT = {}
+
+ATT.PrintName = "Desert Tan"
+ATT.CompactName = "Tan"
+ATT.Icon = Material("models/weapons/arc9/camo/arc9_flatcamo/flat_deserttan.png")
+ATT.Category = "universal_camo"
+ATT.Description = [[Standard Desert Tan camouflage.]]
+ATT.MenuCategory = "ARC-9 - Camos"
+ATT.CustomCamoTexture = "models/weapons/arc9/camo/arc9_flatcamo/vtf/flat_deserttan"
+ATT.CustomCamoScale = 1
+ATT.Folder = "Base/Flat Color/Camouflage"
+
+ARC9.LoadAttachment(ATT, "camo_arc9_base_flat_deserttan")
+
+ATT = {}
+
+ATT.PrintName = "Arctic White"
+ATT.CompactName = "Arctic"
+ATT.Icon = Material("models/weapons/arc9/camo/arc9_flatcamo/flat_arctic.png")
+ATT.Category = "universal_camo"
+ATT.Description = [[Standard Arctic White camouflage.]]
+ATT.MenuCategory = "ARC-9 - Camos"
+ATT.CustomCamoTexture = "models/weapons/arc9/camo/arc9_flatcamo/vtf/flat_arctic"
+ATT.CustomCamoScale = 1
+ATT.Folder = "Base/Flat Color/Camouflage"
+
+ARC9.LoadAttachment(ATT, "camo_arc9_base_flat_arctic")
+
+ATT = {}
+
+ATT.PrintName = "Factory Black"
+ATT.CompactName = "Black"
+ATT.Icon = Material("models/weapons/arc9/camo/arc9_flatcamo/flat_black.png")
+ATT.Category = "universal_camo"
+ATT.Description = [[Standard Factory Issue Black. Maybe camouflage is a little too overrated for you?]]
+ATT.MenuCategory = "ARC-9 - Camos"
+ATT.CustomCamoTexture = "models/weapons/arc9/camo/arc9_flatcamo/vtf/flat_black"
+ATT.CustomCamoScale = 1
+ATT.Folder = "Base/Flat Color/Camouflage"
+
+ARC9.LoadAttachment(ATT, "camo_arc9_base_flat_black")
+
+ATT = {}
+
+ATT.PrintName = "Urban Gray"
+ATT.CompactName = "Urban"
+ATT.Icon = Material("models/weapons/arc9/camo/arc9_flatcamo/flat_urbangray.png")
+ATT.Category = "universal_camo"
+ATT.Description = [[Standard Urban Gray camouflage. (actually Dark Ghost Grey, the aircraft color, but shhhhh.)]]
+ATT.MenuCategory = "ARC-9 - Camos"
+ATT.CustomCamoTexture = "models/weapons/arc9/camo/arc9_flatcamo/vtf/flat_urbangray"
+ATT.CustomCamoScale = 1
+ATT.Folder = "Base/Flat Color/Camouflage"
+
+ARC9.LoadAttachment(ATT, "camo_arc9_base_flat_urbangray")
+
+ATT = {}
+
+ATT.PrintName = "Forest Green"
+ATT.CompactName = "Forest"
+ATT.Icon = Material("models/weapons/arc9/camo/arc9_flatcamo/flat_forestgreen.png")
+ATT.Category = "universal_camo"
+ATT.Description = [[Standard Forest Green camouflage.]]
+ATT.MenuCategory = "ARC-9 - Camos"
+ATT.CustomCamoTexture = "models/weapons/arc9/camo/arc9_flatcamo/vtf/flat_forestgreen"
+ATT.CustomCamoScale = 1
+ATT.Folder = "Base/Flat Color/Camouflage"
+
+ARC9.LoadAttachment(ATT, "camo_arc9_base_flat_forestgreen")
+
+ATT = {}
+
+ATT.PrintName = "Navy Blue"
+ATT.CompactName = "Navy"
+ATT.Icon = Material("models/weapons/arc9/camo/arc9_flatcamo/flat_navy.png")
+ATT.Category = "universal_camo"
+ATT.Description = [[Standard Navy Blue color. Camouflage isn't quite the right word here because Navy is more of a ceremonial color than a camouflage color. Still, you may find it to be visually pleasing.]]
+ATT.MenuCategory = "ARC-9 - Camos"
+ATT.CustomCamoTexture = "models/weapons/arc9/camo/arc9_flatcamo/vtf/flat_navy"
+ATT.CustomCamoScale = 1
+ATT.Folder = "Base/Flat Color/Camouflage"
+
+ARC9.LoadAttachment(ATT, "camo_arc9_base_flat_navy")
+
+ATT = {}
+
+ATT.PrintName = "Mountbatten Pink"
+ATT.CompactName = "Pink"
+ATT.Icon = Material("models/weapons/arc9/camo/arc9_flatcamo/flat_mountbattenpink.png")
+ATT.Category = "universal_camo"
+ATT.Description = [[A curious camouflage color utilized by the British Royal Navy during World War II. Though not used on firearms, it was instead used for battleships and vehicles. It ultimately failed due to it really only being effective at dusk and dawn in the desert.]]
+ATT.MenuCategory = "ARC-9 - Camos"
+ATT.CustomCamoTexture = "models/weapons/arc9/camo/arc9_flatcamo/vtf/flat_mountbattenpink"
+ATT.CustomCamoScale = 1
+ATT.Folder = "Base/Flat Color/Camouflage"
+
+ARC9.LoadAttachment(ATT, "camo_arc9_base_flat_mountbattenpink")
+
+ATT = {}
+
+ATT.PrintName = "Flat Dark Earth"
+ATT.CompactName = "FDE"
+ATT.Icon = Material("models/weapons/arc9/camo/arc9_flatcamo/flat_fde.png")
+ATT.Category = "universal_camo"
+ATT.Description = [[Standard Flat Dark Earth camouflage.]]
+ATT.MenuCategory = "ARC-9 - Camos"
+ATT.CustomCamoTexture = "models/weapons/arc9/camo/arc9_flatcamo/vtf/flat_fde"
+ATT.CustomCamoScale = 1
+ATT.Folder = "Base/Flat Color/Camouflage"
+
+ARC9.LoadAttachment(ATT, "camo_arc9_base_flat_fde")
---
+++ lua/arc9/common/attachments_bulk/camos_base_flat_color.lua
@@ -0,0 +1,111 @@
+local ATT = {}
+
+ATT.PrintName = "Red"
+ATT.CompactName = "Red"
+ATT.Icon = Material("models/weapons/arc9/camo/arc9_flatcamo/flat_red.png")
+ATT.Category = "universal_camo"
+ATT.Description = [[Basic red color.]]
+ATT.MenuCategory = "ARC-9 - Camos"
+ATT.CustomCamoTexture = "models/weapons/arc9/camo/arc9_flatcamo/vtf/flat_red"
+ATT.CustomCamoScale = 1
+ATT.Folder = "Base/Flat Color/Color"
+
+ARC9.LoadAttachment(ATT, "camo_arc9_base_flat_red")
+
+ATT = {}
+
+ATT.PrintName = "Orange"
+ATT.CompactName = "Orange"
+ATT.Icon = Material("models/weapons/arc9/camo/arc9_flatcamo/flat_orange.png")
+ATT.Category = "universal_camo"
+ATT.Description = [[Basic orange color.]]
+ATT.MenuCategory = "ARC-9 - Camos"
+ATT.CustomCamoTexture = "models/weapons/arc9/camo/arc9_flatcamo/vtf/flat_orange"
+ATT.CustomCamoScale = 1
+ATT.Folder = "Base/Flat Color/Color"
+
+ARC9.LoadAttachment(ATT, "camo_arc9_base_flat_orange")
+
+ATT = {}
+
+ATT.PrintName = "Yellow"
+ATT.CompactName = "Yellow"
+ATT.Icon = Material("models/weapons/arc9/camo/arc9_flatcamo/flat_yellow.png")
+ATT.Category = "universal_camo"
+ATT.Description = [[Basic yellow color.]]
+ATT.MenuCategory = "ARC-9 - Camos"
+ATT.CustomCamoTexture = "models/weapons/arc9/camo/arc9_flatcamo/vtf/flat_yellow"
+ATT.CustomCamoScale = 1
+ATT.Folder = "Base/Flat Color/Color"
+
+ARC9.LoadAttachment(ATT, "camo_arc9_base_flat_yellow")
+
+ATT = {}
+
+ATT.PrintName = "Green"
+ATT.CompactName = "Green"
+ATT.Icon = Material("models/weapons/arc9/camo/arc9_flatcamo/flat_green.png")
+ATT.Category = "universal_camo"
+ATT.Description = [[Basic green color.]]
+ATT.MenuCategory = "ARC-9 - Camos"
+ATT.CustomCamoTexture = "models/weapons/arc9/camo/arc9_flatcamo/vtf/flat_green"
+ATT.CustomCamoScale = 1
+ATT.Folder = "Base/Flat Color/Color"
+
+ARC9.LoadAttachment(ATT, "camo_arc9_base_flat_green")
+
+ATT = {}
+
+ATT.PrintName = "Blue"
+ATT.CompactName = "Blue"
+ATT.Icon = Material("models/weapons/arc9/camo/arc9_flatcamo/flat_blue.png")
+ATT.Category = "universal_camo"
+ATT.Description = [[Basic blue color.]]
+ATT.MenuCategory = "ARC-9 - Camos"
+ATT.CustomCamoTexture = "models/weapons/arc9/camo/arc9_flatcamo/vtf/flat_blue"
+ATT.CustomCamoScale = 1
+ATT.Folder = "Base/Flat Color/Color"
+
+ARC9.LoadAttachment(ATT, "camo_arc9_base_flat_blue")
+
+ATT = {}
+
+ATT.PrintName = "Purple"
+ATT.CompactName = "Purple"
+ATT.Icon = Material("models/weapons/arc9/camo/arc9_flatcamo/flat_purple.png")
+ATT.Category = "universal_camo"
+ATT.Description = [[Basic purple color.]]
+ATT.MenuCategory = "ARC-9 - Camos"
+ATT.CustomCamoTexture = "models/weapons/arc9/camo/arc9_flatcamo/vtf/flat_purple"
+ATT.CustomCamoScale = 1
+ATT.Folder = "Base/Flat Color/Color"
+
+ARC9.LoadAttachment(ATT, "camo_arc9_base_flat_purple")
+
+ATT = {}
+
+ATT.PrintName = "Pink"
+ATT.CompactName = "Pink"
+ATT.Icon = Material("models/weapons/arc9/camo/arc9_flatcamo/flat_pink.png")
+ATT.Category = "universal_camo"
+ATT.Description = [[Eye searingly bright pink color.]]
+ATT.MenuCategory = "ARC-9 - Camos"
+ATT.CustomCamoTexture = "models/weapons/arc9/camo/arc9_flatcamo/vtf/flat_pink"
+ATT.CustomCamoScale = 1
+ATT.Folder = "Base/Flat Color/Color"
+
+ARC9.LoadAttachment(ATT, "camo_arc9_base_flat_pink")
+
+ATT = {}
+
+ATT.PrintName = "ARC9 Orange"
+ATT.CompactName = "ARC9 Orange"
+ATT.Icon = Material("models/weapons/arc9/camo/arc9_flatcamo/flat_arc9orange.png")
+ATT.Category = "universal_camo"
+ATT.Description = [[The perfect shade of orange for that cute little ARC9 Logo sticker.]]
+ATT.MenuCategory = "ARC-9 - Camos"
+ATT.CustomCamoTexture = "models/weapons/arc9/camo/arc9_flatcamo/vtf/flat_arc9orange"
+ATT.CustomCamoScale = 1
+ATT.Folder = "Base/Flat Color/Color"
+
+ARC9.LoadAttachment(ATT, "camo_arc9_base_flat_arc9orange")
