613ad5884f8ec36da3d1a17ccdf0d605a6434625
TheOnly8Z <888888zombies@gmail.com>
2023-01-27 21:55:46 -0600

    why would you do that??????

--- lua/weapons/arc9_base/sh_init.lua
+++ lua/weapons/arc9_base/sh_init.lua
@@ -32,16 +32,10 @@ function SWEP:Initialize()
     self.DefaultAttachments = table.Copy(self.Attachments)
 
     self:BuildSubAttachments(self.DefaultAttachments)
-    
+
     if !IsValid(owner) then -- player is nil here sometimes
         self:PostModify()
     end
-
-    timer.Simple(0, function()
-        if IsValid(self) and !IsValid(owner) then -- we are on ground!
-            self:SetClip1(self:GetValue("ClipSize") * 3) -- *3 better be something own please fix later
-        end
-    end)
 
     self.LastClipSize = self:GetProcessedValue("ClipSize")
 
