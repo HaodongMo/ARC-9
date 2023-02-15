c5b9cd79c55bae2adf1cf897ad88e84a317b9dc5
Darsu <95997959+dar-su@users.noreply.github.com>
2023-02-15 15:11:50 +0700

    SWEP.NoForceSetLoadedRoundsOnReload -- sorry

--- lua/weapons/arc9_base/sh_reload.lua
+++ lua/weapons/arc9_base/sh_reload.lua
@@ -87,7 +87,6 @@ function SWEP:Reload()
         end
 
         anim = nanim
-
     end
 
     if !self:GetProcessedValue("ReloadInSights") then
@@ -143,7 +142,9 @@ function SWEP:Reload()
         self:Unload()
     end
 
-    self:SetLoadedRounds(self:Clip1())
+    if !self.NoForceSetLoadedRoundsOnReload then -- sorry
+        self:SetLoadedRounds(self:Clip1())
+    end
 
     self:SetReloading(true)
     self:SetEndReload(false)
@@ -438,10 +439,11 @@ function SWEP:EndReload()
 
             local magswaptime = (self:GetAnimationEntry(anim) or {}).MagSwapTime or 0
 
-            self:SetTimer(magswaptime * t, function()
-                self:SetLoadedRounds(end_clipsize)
-            end)
-
+            if !self.NoForceSetLoadedRoundsOnReload then -- sorry
+                self:SetTimer(magswaptime * t, function()
+                    self:SetLoadedRounds(end_clipsize)
+                end)
+            end
 
             self:SetTimer(minprogress * t, function()
                 self:RestoreClip(attempt_to_restore)
