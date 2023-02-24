f38e7f8012cc9e783b0e70e116fcedf8f41944ce
Darsu <95997959+dar-su@users.noreply.github.com>
2023-02-24 20:39:10 +0700

    slowmo sounds like tfa real

--- lua/arc9/shared/sh_gunutils.lua
+++ lua/arc9/shared/sh_gunutils.lua
@@ -1,11 +1,11 @@
 gameevent.Listen( "entity_killed" )
 hook.Add( "entity_killed", "entity_killed_example", function( data ) 
-    local inflictor_index = data.entindex_inflictor		// Same as Weapon:EntIndex() / weapon used to kill victim
-    local attacker_index = data.entindex_attacker		// Same as Player/Entity:EntIndex() / person or entity who did the damage
-    local damagebits = data.damagebits			// DAMAGE_TYPE - use BIT operations to decipher damage types...
-    local victim_index = data.entindex_killed		// Same as Victim:EntIndex() / the entity / player victim
+    local inflictor_index = data.entindex_inflictor		-- Same as Weapon:EntIndex() / weapon used to kill victim
+    local attacker_index = data.entindex_attacker		-- Same as Player/Entity:EntIndex() / person or entity who did the damage
+    local damagebits = data.damagebits			-- DAMAGE_TYPE - use BIT operations to decipher damage types...
+    local victim_index = data.entindex_killed		-- Same as Victim:EntIndex() / the entity / player victim
 
-    // Called when a Player or Entity is killed
+    -- Called when a Player or Entity is killed
 
     if !victim_index then return end
     if !attacker_index then return end
@@ -25,3 +25,34 @@ hook.Add( "entity_killed", "entity_killed_example", function( data )
         end
     end
 end )
+
+timer.Simple(10, function() -- tfa does same thing, no need to copy (timer here cuz tfa loads after arc9)
+    if !TFA then 
+        -- code stolen from wiki
+        local cheats = GetConVar("sv_cheats" )
+        local timeScale = GetConVar("host_timescale" )
+
+        hook.Add("EntityEmitSound", "ARC9_TimeWarpSounds", function(t)
+            local p = t.Pitch
+            
+            if game.GetTimeScale() != 1 then
+                p = p * game.GetTimeScale()
+            end
+            
+            if timeScale:GetInt() != 1 and cheats >= 1 then
+                p = p * timeScale:GetInt()
+            end
+            
+            if p != t.Pitch then
+                t.Pitch = math.Clamp(p, 0, 255)
+                return true
+            end
+            
+            if CLIENT and engine.GetDemoPlaybackTimeScale() != 1 then
+                t.Pitch = math.Clamp(t.Pitch * engine.GetDemoPlaybackTimeScale(), 0, 255)
+                return true
+            end
+            
+        end)
+    end
+end)
