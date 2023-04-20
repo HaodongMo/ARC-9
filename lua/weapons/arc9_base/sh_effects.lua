fd6f2258ad2852b1e384d87498521e4d4b73c1aa
Darsu <95997959+dar-su@users.noreply.github.com>
2023-04-21 00:45:49 +0700

    try

--- lua/weapons/arc9_base/sh_effects.lua
+++ lua/weapons/arc9_base/sh_effects.lua
@@ -60,7 +60,7 @@ end
 
 function SWEP:GetTracerOrigin()
     local ow = self:GetOwner()
-    local vm = !ow:IsNPC() and ow:GetViewModel() or nil
+    local vm = ow.GetViewModel and ow:GetViewModel() or nil
     local wm = ow:IsNPC() or !ow:IsValid() or !vm:IsValid() or ow:ShouldDrawLocalPlayer()
     local att = self:GetQCAMuzzle()
     local muzz = self
