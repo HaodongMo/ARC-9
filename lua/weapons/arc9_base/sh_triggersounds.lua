139c709969fb26123ba06190c4be775cff4a9bd4
Haodong Mo <mohaodong@gmail.com>
2023-04-23 14:01:57 +1000

    Attempted fix to trigger sounds

--- lua/weapons/arc9_base/sh_triggersounds.lua
+++ lua/weapons/arc9_base/sh_triggersounds.lua
@@ -4,6 +4,10 @@ function SWEP:ThinkTriggerSounds()
     if self:SprintLock() then return end
     if self:GetSafe() then return end
     local owner = self:GetOwner()
+    local processedValue = self.GetProcessedValue
+
+    if processedValue(self,"Throwable", true) then return end
+    if processedValue(self,"PrimaryBash", true) then return end
 
     if owner:KeyReleased(IN_ATTACK) then
         local soundtab = {
@@ -14,6 +18,8 @@ function SWEP:ThinkTriggerSounds()
 
         self:PlayTranslatedSound(soundtab)
     elseif owner:KeyPressed(IN_ATTACK) then
+        if processedValue(self,"Bash", true) and owner:KeyDown(IN_USE) and !self:GetInSights() then return end
+
         local soundtab = {
             name = "triggerdown",
             sound = self:RandomChoice(self:GetProcessedValue("TriggerDownSound", true)),
