d055ef0b946390c935a19ea63723aae8300463ae
Darsu <95997959+dar-su@users.noreply.github.com>
2023-05-20 21:43:22 +0700

    actual retards

--- lua/arc9/client/cl_bind.lua
+++ lua/arc9/client/cl_bind.lua
@@ -77,6 +77,8 @@ function ARC9.GetBindKey(bind)
     if CTRL then
         return bind
     elseif !key then
+        if bind == "+zoom" then return "pls bind Suit Zoom!" end -- fucking blind stupid retards please open settings
+        if bind == "+menu_context" then return "pls bind Open Context Menu!" end
         return "bind KEY " ..  bind
     else
         return string.upper(key)
