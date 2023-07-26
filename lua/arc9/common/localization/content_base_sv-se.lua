9bba7dc17e86866c13f065bd257562236739ad50
MokaAkashiya85 <ThaERROR404@gmail.com>
2023-07-26 16:52:03 +0200

    Camo, Sticker and Charm translations
    - Added localizations for all included camouflages, stickers and charms used by the main base. Slightly adjusted some localizations alongside, but kept most, if not all of them, original.
    - Also translated it to Swedish.

---
+++ lua/arc9/common/localization/content_base_sv-se.lua
@@ -0,0 +1,274 @@
+L = {}
+
+-- Flat Camo
+L["camo_arc9_base_flat_od.printname"] = "Olivgrön"
+L["camo_arc9_base_flat_od.compactname"] = "OG"
+L["camo_arc9_base_flat_od.description"] = "Standard Olivgrön kamouflage."
+
+L["camo_arc9_base_flat_deserttan.printname"] = "Gulbrun Öken"
+L["camo_arc9_base_flat_deserttan.compactname"] = "Öken"
+L["camo_arc9_base_flat_deserttan.description"] = "Standard Gulbrun Ökenkamouflage."
+
+L["camo_arc9_base_flat_arctic.printname"] = "Arktiskt Vit"
+L["camo_arc9_base_flat_arctic.compactname"] = "Arktisk"
+L["camo_arc9_base_flat_arctic.description"] = "Standard Arktiskt Vit kamouflage."
+
+L["camo_arc9_base_flat_black.printname"] = "Fabrikssvart"
+L["camo_arc9_base_flat_black.compactname"] = "Svart"
+L["camo_arc9_base_flat_black.description"] = "Standard Fabrikssvart."
+
+L["camo_arc9_base_flat_urbangray.printname"] = "Stadsgrå"
+L["camo_arc9_base_flat_urbangray.compactname"] = "Stad"
+L["camo_arc9_base_flat_urbangray.description"] = "Standard Stadsgrå kamouflage."
+
+L["camo_arc9_base_flat_forestgreen.printname"] = "Skogsgrön"
+L["camo_arc9_base_flat_forestgreen.compactname"] = "Skog"
+L["camo_arc9_base_flat_forestgreen.description"] = "Standard Skogsgrön kamouflage."
+
+L["camo_arc9_base_flat_navy.printname"] = "Marinblå"
+L["camo_arc9_base_flat_navy.compactname"] = "Marin"
+L["camo_arc9_base_flat_navy.description"] = "Standard Marinblå färg."
+
+L["camo_arc9_base_flat_mountbattenpink.printname"] = "Mountbatten Pink"
+L["camo_arc9_base_flat_mountbattenpink.compactname"] = "MP"
+L["camo_arc9_base_flat_mountbattenpink.description"] = "Standard Mountbatten Pink kamouflage.\nChockande effektiv i ökenomgivning."
+
+L["camo_arc9_base_flat_fde.printname"] = "Platt Mörk Jord"
+L["camo_arc9_base_flat_fde.compactname"] = "PMJ"
+L["camo_arc9_base_flat_fde.description"] = "Standard Platt Mörk Jord kamouflage."
+
+-- Flat Colour
+L["camo_arc9_base_flat_red.printname"] = "Röd"
+L["camo_arc9_base_flat_red.compactname"] = "Röd"
+L["camo_arc9_base_flat_red.description"] = "Simpel röd färg."
+
+L["camo_arc9_base_flat_orange.printname"] = "Orange"
+L["camo_arc9_base_flat_orange.compactname"] = "Orange"
+L["camo_arc9_base_flat_orange.description"] = "Simpel orange färg."
+
+L["camo_arc9_base_flat_yellow.printname"] = "Gul"
+L["camo_arc9_base_flat_yellow.compactname"] = "Gul"
+L["camo_arc9_base_flat_yellow.description"] = "Simpel gul färg."
+
+L["camo_arc9_base_flat_green.printname"] = "Grön"
+L["camo_arc9_base_flat_green.compactname"] = "Grön"
+L["camo_arc9_base_flat_green.description"] = "Simpel grön färg."
+
+L["camo_arc9_base_flat_blue.printname"] = "Blå"
+L["camo_arc9_base_flat_blue.compactname"] = "Blå"
+L["camo_arc9_base_flat_blue.description"] = "Simpel blå färg."
+
+L["camo_arc9_base_flat_purple.printname"] = "Lila"
+L["camo_arc9_base_flat_purple.compactname"] = "Lila"
+L["camo_arc9_base_flat_purple.description"] = "Simpel lila färg."
+
+L["camo_arc9_base_flat_pink.printname"] = "Rosa"
+L["camo_arc9_base_flat_pink.compactname"] = "Rosa"
+L["camo_arc9_base_flat_pink.description"] = "Ögon brännande stark rosa färg."
+
+L["camo_arc9_base_flat_arc9orange.printname"] = "ARC9-Orange"
+L["camo_arc9_base_flat_arc9orange.compactname"] = "ARC9-Orange"
+L["camo_arc9_base_flat_arc9orange.description"] = "Den perfekta nyans av orange för den söta lilla ARC9-logga klistermärket."
+
+-- Charms
+L["charm_gs_killcounter.printname"] = "Dråp Räknare"
+L["charm_gs_killcounter.compactname"] = "DRÅP"
+L["charm_gs_killcounter.description"] = "Enhet som spårar ditt vapens dråp statistik."
+
+L["charm_gs_clock.printname"] = "Simpel Klocka"
+L["charm_gs_clock.compactname"] = "KLOCKA"
+L["charm_gs_clock.description"] = "Device that displays the real-world time."
+L["charm_gs_clock.description"] = "Enhet som visar tiden i realtid."
+
+L["charm_gs_sticker.printname"] = "Klistermärke Panel"
+L["charm_gs_sticker.compactname"] = "KLISTER."
+L["charm_gs_sticker.description"] = "Enhet som tillåter en att sätta sitt egna klistermärke på dess skärm."
+
+-- Stickers, Base
+L["sticker_spray.printname"] = "Spelare Sprej"
+L["sticker_spray.compactname"] = "Sprej"
+L["sticker_spray.description"] = [[Din egna sprej som ett klistermärkte!
+Du kommer endast kunna se din egna och inte några från andra spelare.
+
+Klistermärke inkluderad i ARC9-basen.]]
+
+L["sticker_arc9.printname"] = "ARC9-Logga (Låg Syn.)"
+L["sticker_arc9.compactname"] = "ARC9"
+L["sticker_arc9.description"] = [[Mörk version av ARC9-loggan.
+Design av The Villain.
+
+Klistermärke inkluderad i ARC9-basen.]]
+
+L["sticker_arc9_lowvis.printname"] = "ARC9-Logga"
+L["sticker_arc9_lowvis.compactname"] = "ARC9"
+L["sticker_arc9_lowvis.description"] = [[Till dig.
+Design av The Villain.
+
+Klistermärke inkluderad i ARC9-basen.]]
+
+L["sticker_a9k.printname"] = "ARCTICTHRON 9.000 WEAPON BASE"
+L["sticker_a9k.compactname"] = "A9K"
+L["sticker_a9k.description"] = [[detta är vad "arc9" egentligen betyder
+
+Klistermärke inkluderad i ARC9-basen.]]
+
+L["sticker_a9cylo.printname"] = "ARC9-Logga koncept av CyloWalker"
+L["sticker_a9cylo.compactname"] = "A9 Cylo"
+L["sticker_a9cylo.description"] = [[En av dem första koncepten för ARC9-loggan/ikonen av CyloWalker (han är schysst!)
+
+Klistermärke inkluderad i ARC9-basen.]]
+
+L["sticker_sus9.printname"] = "Sus 9"
+L["sticker_sus9.compactname"] = "Sus 9"
+L["sticker_sus9.description"] = [[Den misstänkte kan vara vart som helst bland oss...
+Design av Darsu.
+
+Klistermärke inkluderad i ARC9-basen.]]
+
+-- Stickers, Com. Wave 1
+L["sticker_alien_lean.printname"] = "Legalisera Utomjording Lutning"
+L["sticker_alien_lean.compactname"] = "LUT"
+L["sticker_alien_lean.description"] = [[Hoppa ner till Agartha med den vita pojkarna för en kopp av utomjordiskt lutning och... andra grejer.
+Design av Opt1ca.
+
+Klistermärke inkluderad i ARC9-basen.]]
+
+L["sticker_tomatocat.printname"] = "Tomat Katt"
+L["sticker_tomatocat.compactname"] = "Tomat Katt"
+L["sticker_tomatocat.description"] = [[Ibland frågar folk om tomater är frukter eller grönsaker... detta kan vara en tuff fråga.
+Design av Ender2Point0.
+
+Klistermärke inkluderad i ARC9-basen.]]
+
+L["sticker_actionbird.printname"] = "Handling Fågel"
+L["sticker_actionbird.compactname"] = "Handling Fågel"
+L["sticker_actionbird.description"] = [[Detta är hur Arctic ser ut som på verkligt.
+Design av CyloWalker.
+
+Klistermärke inkluderad i ARC9-basen.]]
+
+L["sticker_warcrimes.printname"] = "Krigsbrott"
+L["sticker_warcrimes.compactname"] = "Krigsbrott"
+L["sticker_warcrimes.description"] = [[Du utför dem redan, så varför inte skrika högt om det? Vem ska stoppa dig, FN?
+Design av Rimuchii. Gjord med Project Sekai sticker base.
+
+Klistermärke inkluderad i ARC9-basen.]]
+
+L["sticker_sillycat.printname"] = "Dum Katt"
+L["sticker_sillycat.compactname"] = "Dum Katt"
+L["sticker_sillycat.description"] = [[jagärtokigjagärtokigjagärtokigjagärtokigjagärtokig
+Design av Stan_Jacobs.
+
+Klistermärke inkluderad i ARC9-basen.]]
+
+L["sticker_muertosskull.printname"] = "Dia De Los Muertos Calavera"
+L["sticker_muertosskull.compactname"] = "Los Muertos"
+L["sticker_muertosskull.description"] = [["detta borde vara min sista version tror jag, om inte någon ger mig föreslag på att ändra den, namnet: "Dia de los muertos calavera" beskrivningen: kan inte tänka mig en, något om dem dödas dag i mexiko blah blah blah"
+Design av rooneyviz.
+
+Klistermärke inkluderad i ARC9-basen.]]
+
+L["sticker_bandaid.printname"] = "Plåster"
+L["sticker_bandaid.compactname"] = "Plåster"
+L["sticker_bandaid.description"] = [[Sätt den på ditt sår och pussa på den tills det blir bättre.
+Design av Itzal.
+
+Klistermärke inkluderad i ARC9-basen.]]
+
+L["sticker_peacedove.printname"] = "Fred Genom Överlägsen Eldkraft"
+L["sticker_peacedove.compactname"] = "FGÖE"
+L["sticker_peacedove.description"] = [[Duvan ger frid, men vapnet ger frid genom överlägsen eldkraft.
+Design av Xipil.
+
+Klistermärke inkluderad i ARC9-basen.]]
+
+L["sticker_borntomodify.printname"] = "Född Att Anpassa"
+L["sticker_borntomodify.compactname"] = "FAA"
+L["sticker_borntomodify.description"] = [[Ännu ett sikte - ännu ett kikarsikte - en granattillsats, här och där...
+Design av Duck.
+
+Klistermärke inkluderad i ARC9-basen.]]
+
+L["sticker_cad.printname"] = "cad"
+L["sticker_cad.compactname"] = "cad"
+L["sticker_cad.description"] = [[cad
+Design av Opt1ca.
+
+Klistermärke inkluderad i ARC9-basen.]]
+
+L["sticker_fumo.printname"] = "Fumo"
+L["sticker_fumo.compactname"] = "Fumo"
+L["sticker_fumo.description"] = [[T-Titta inte på mig sådär, raifu-kun...
+Design av Itzal.
+
+Klistermärke inkluderad i ARC9-basen.]]
+
+L["sticker_hotlead.printname"] = "Varmt Bly"
+L["sticker_hotlead.compactname"] = "Varmt Bly"
+L["sticker_hotlead.description"] = [[Försiktigt! Det är varmt!
+Design av Arqu.
+
+Klistermärke inkluderad i ARC9-basen.]]
+
+L["sticker_steamhappy.printname"] = ":steamhappy:"
+L["sticker_steamhappy.compactname"] = ":steamhappy:"
+L["sticker_steamhappy.description"] = [[Glad om det
+Design från Steam.
+
+Klistermärke inkluderad i ARC9-basen.]]
+
+L["sticker_amidstus.printname"] = "Amidstus"
+L["sticker_amidstus.compactname"] = "Amidstus"
+L["sticker_amidstus.description"] = [[Varför nöja sig med originalet när man kan ha en billig knockoff?!
+Design från Optica.
+
+Klistermärke inkluderad i ARC9-basen.]]
+
+L["sticker_arcglory.printname"] = "Ära!"
+L["sticker_arcglory.compactname"] = "Ära!"
+L["sticker_arcglory.description"] = [[Krävde nio hjärnceller att göra detta
+Design från Optica.
+
+Klistermärke inkluderad i ARC9-basen.]]
+
+L["sticker_wp.printname"] = "Världsfred!"
+L["sticker_wp.compactname"] = "Världsfred!"
+L["sticker_wp.description"] = [[Villfarelse!
+Design från Fidget.
+
+Klistermärke inkluderad i ARC9-basen.]]
+
+L["sticker_tonkr.printname"] = "Skicka Det"
+L["sticker_tonkr.compactname"] = "Skicka Det"
+L["sticker_tonkr.description"] = [[Ja, den är så tung. Ja, den är så kraftfull.
+Design från CyloWalker.
+
+Klistermärke inkluderad i ARC9-basen.]]
+
+L["sticker_s6amblem.printname"] = "Sektion 6"
+L["sticker_s6amblem.compactname"] = "Sektion 6"
+L["sticker_s6amblem.description"] = [[Sikte mot stjärnorna. Framåt mot äran!
+Design från CyloWalker.
+
+Klistermärke inkluderad i ARC9-basen.]]
+
+L["sticker_babies.printname"] = "Fara för Bäbisar"
+L["sticker_babies.compactname"] = "Fara"
+L["sticker_babies.description"] = [[Barn i åldrarna 0-3 borde hållas borta från detta... skjutvapnet.
+Design från Itzal.
+
+Klistermärke inkluderad i ARC9-basen.]]
+
+L["sticker_drawberf.printname"] = "Berf"
+L["sticker_drawberf.compactname"] = "Berf"
+L["sticker_drawberf.description"] = [[Rita Berf.
+Design av The Villain.
+
+Klistermärke inkluderad i ARC9-basen.]]
+
+L["sticker_arcbird.printname"] = "Arc-Fågel"
+L["sticker_arcbird.compactname"] = "Arc-Fågel"
+L["sticker_arcbird.description"] = [[Arc-Fågel.
+Design av Nori/Chewable.
+
+Klistermärke inkluderad i ARC9-basen.]]
