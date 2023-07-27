8025897a07d3acd16c84e1d36c5631d102737883
MokaAkashiya85 <ThaERROR404@gmail.com>
2023-07-27 17:02:05 +0200

    Create content_base_ru.lua
    - Added Russian translations for ARC9 camos, stickers and charms (Credit to Blueberry on Discord for it)

---
+++ lua/arc9/common/localization/content_base_ru.lua
@@ -0,0 +1,273 @@
+L = {}
+
+-- Flat Camo
+L["camo_arc9_base_flat_od.printname"] = "Сплошной оливковый"
+L["camo_arc9_base_flat_od.compactname"] = "СО"
+L["camo_arc9_base_flat_od.description"] = "Стандартный сплошной оливковый камуфляж."
+
+L["camo_arc9_base_flat_deserttan.printname"] = "Светло-коричневый пустынный"
+L["camo_arc9_base_flat_deserttan.compactname"] = "Светло-коричневый"
+L["camo_arc9_base_flat_deserttan.description"] = "Стандартный светло-коричневый пустынный камуфляж."
+
+L["camo_arc9_base_flat_arctic.printname"] = "Арктический белый"
+L["camo_arc9_base_flat_arctic.compactname"] = "Арктический"
+L["camo_arc9_base_flat_arctic.description"] = "Стандартный арктический белый камуфляж."
+
+L["camo_arc9_base_flat_black.printname"] = "Заводской чёрный"
+L["camo_arc9_base_flat_black.compactname"] = "Чёрный"
+L["camo_arc9_base_flat_black.description"] = "Стандартный заводской чёрный вариант."
+
+L["camo_arc9_base_flat_urbangray.printname"] = "Городской серый"
+L["camo_arc9_base_flat_urbangray.compactname"] = "Городской"
+L["camo_arc9_base_flat_urbangray.description"] = "Стандартный городской серый камуфляж."
+
+L["camo_arc9_base_flat_forestgreen.printname"] = "Лесная зелень"
+L["camo_arc9_base_flat_forestgreen.compactname"] = "Лесная"
+L["camo_arc9_base_flat_forestgreen.description"] = "Стандартная камуфляж лесной зелени."
+
+L["camo_arc9_base_flat_navy.printname"] = "Морская синева"
+L["camo_arc9_base_flat_navy.compactname"] = "Морская"
+L["camo_arc9_base_flat_navy.description"] = "Стандартный камуфляж морской синевы."
+
+L["camo_arc9_base_flat_mountbattenpink.printname"] = "Розовый Маунтбэттена"
+L["camo_arc9_base_flat_mountbattenpink.compactname"] = "РМ"
+L["camo_arc9_base_flat_mountbattenpink.description"] = "Стандартный розовый камуфляж Маунтбэттена.\nПотрясающе эффективен в условиях пустыни."
+
+L["camo_arc9_base_flat_fde.printname"] = "Плоская тёмная земля"
+L["camo_arc9_base_flat_fde.compactname"] = "ПТЗ"
+L["camo_arc9_base_flat_fde.description"] = "Стандартный плоский тЁмно-земляной камуфляж."
+
+-- Flat Colour
+L["camo_arc9_base_flat_red.printname"] = "Красный"
+L["camo_arc9_base_flat_red.compactname"] = "Красный"
+L["camo_arc9_base_flat_red.description"] = "Обычный красный цвет."
+
+L["camo_arc9_base_flat_orange.printname"] = "Оранжевый"
+L["camo_arc9_base_flat_orange.compactname"] = "Оранжевый"
+L["camo_arc9_base_flat_orange.description"] = "Обычный оранжевый цвет."
+
+L["camo_arc9_base_flat_yellow.printname"] = "Жёлтый"
+L["camo_arc9_base_flat_yellow.compactname"] = "Жёлтый"
+L["camo_arc9_base_flat_yellow.description"] = "Обычный жёлтый цвет."
+
+L["camo_arc9_base_flat_green.printname"] = "Зелёный"
+L["camo_arc9_base_flat_green.compactname"] = "Зелёный"
+L["camo_arc9_base_flat_green.description"] = "Обычный зелёный цвет."
+
+L["camo_arc9_base_flat_blue.printname"] = "Синий"
+L["camo_arc9_base_flat_blue.compactname"] = "Синий"
+L["camo_arc9_base_flat_blue.description"] = "Обычный синий цвет."
+
+L["camo_arc9_base_flat_purple.printname"] = "Фиолетовый"
+L["camo_arc9_base_flat_purple.compactname"] = "Фиолетовый"
+L["camo_arc9_base_flat_purple.description"] = "Обычный фиолетовый цвет."
+
+L["camo_arc9_base_flat_pink.printname"] = "Розовый"
+L["camo_arc9_base_flat_pink.compactname"] = "Розовый"
+L["camo_arc9_base_flat_pink.description"] = "Обжигающий глаз ярко-розовый цвет."
+
+L["camo_arc9_base_flat_arc9orange.printname"] = "Оранжевый - ARC9"
+L["camo_arc9_base_flat_arc9orange.compactname"] = "Оранжевый - ARC9"
+L["camo_arc9_base_flat_arc9orange.description"] = "Идеальный оранжевый оттенок для маленькой симпатичной наклейки с логотипом ARC9."
+
+-- Charms
+L["charm_gs_killcounter.printname"] = "Счётчик убийств"
+L["charm_gs_killcounter.compactname"] = "УБИЙСТВА"
+L["charm_gs_killcounter.description"] = "Устройство для отслеживания статистики убийств с оружия."
+
+L["charm_gs_clock.printname"] = "Обычные часы"
+L["charm_gs_clock.compactname"] = "ЧАСЫ"
+L["charm_gs_clock.description"] = "Устройство, отображающее реальное время."
+
+L["charm_gs_sticker.printname"] = "Панель с наклейками"
+L["charm_gs_sticker.compactname"] = "НАКЛЕЙКА"
+L["charm_gs_sticker.description"] = "Устройство, позволяющее наносить на экран наклейки."
+
+-- Stickers, Base
+L["sticker_spray.printname"] = "Спрей игрока"
+L["sticker_spray.compactname"] = "Спрей"
+L["sticker_spray.description"] = [[Ваш собственный спрей в виде наклейки!
+Вы сможете видеть только свои собственные, а не чужие.
+
+Наклейка входит в состав базы ARC9.]]
+
+L["sticker_arc9.printname"] = "Логотип ARC9 (малозаметный)"
+L["sticker_arc9.compactname"] = "ARC9"
+L["sticker_arc9.description"] = [[Тёмная версия логотипа ARC9.
+Автор: The Villain.
+
+Наклейка входит в состав базы ARC9.]]
+
+L["sticker_arc9_lowvis.printname"] = "Логотип ARC9"
+L["sticker_arc9_lowvis.compactname"] = "ARC9"
+L["sticker_arc9_lowvis.description"] = [[Выпьем за вас.
+Автор: The Villain.
+
+Наклейка входит в состав базы ARC9.]]
+
+L["sticker_a9k.printname"] = "БАЗА ОРУЖИЯ АРКТИКТРОН 9,000"
+L["sticker_a9k.compactname"] = "A9K"
+L["sticker_a9k.description"] = [[вот что на самом деле означает «arc9»
+
+Наклейка входит в состав базы ARC9.]]
+
+L["sticker_a9cylo.printname"] = "Логотип концепции ARC9 от CyloWalker"
+L["sticker_a9cylo.compactname"] = "A9 Cylo"
+L["sticker_a9cylo.description"] = [[Один из первых концептов логотипа/значка ARC9 от CyloWalker (он крут!)
+
+Наклейка входит в состав базы ARC9.]]
+
+L["sticker_sus9.printname"] = "Сас 9"
+L["sticker_sus9.compactname"] = "Сас 9"
+L["sticker_sus9.description"] = [[Предатель может находиться среди нас...
+Автор: Darsu.
+
+Наклейка входит в состав базы ARC9.]]
+
+-- Stickers, Com. Wave 1
+L["sticker_alien_lean.printname"] = "Легализация инопланетного лина"
+L["sticker_alien_lean.compactname"] = "Инопланетный лин"
+L["sticker_alien_lean.description"] = [[Отправляйтесь на Агарту вместе с белыми парнями, чтобы выпить чашечку инопланетного лина и... другие вещи.
+Автор: Opt1ca.
+
+Наклейка входит в состав базы ARC9.]]
+
+L["sticker_tomatocat.printname"] = "Томатный кот"
+L["sticker_tomatocat.compactname"] = "Томатный кот"
+L["sticker_tomatocat.description"] = [[Иногда люди спрашивают, являются ли помидоры фруктами или овощами... Этот вопрос может быть сложнее.
+Автор: Ender2Point0.
+
+Наклейка входит в состав базы ARC9.]]
+
+L["sticker_actionbird.printname"] = "Боевик-птица"
+L["sticker_actionbird.compactname"] = "Боевик-птица"
+L["sticker_actionbird.description"] = [[Вот как выглядит Arctic в реальной жизни.
+Автор: CyloWalker.
+
+Наклейка входит в состав базы ARC9.]]
+
+L["sticker_warcrimes.printname"] = "Военные преступления"
+L["sticker_warcrimes.compactname"] = "Военные преступления"
+L["sticker_warcrimes.description"] = [[Вы уже совершаете это, так почему бы не заявить об этом во всеуслышание? Кто вас остановит, ООН?
+Автор: Rimuchii. Выполнено с использованием базы наклеек Project Sekai.
+
+Наклейка входит в состав базы ARC9.]]
+
+L["sticker_sillycat.printname"] = "Глупая кошка"
+L["sticker_sillycat.compactname"] = "Глупая кошка"
+L["sticker_sillycat.description"] = [[глупышкаглупышкаглупышкаглупышкаглупышкаглупышкаглупышкаглупышка
+Автор: Stan_Jacobs.
+
+Наклейка входит в состав базы ARC9.]]
+
+L["sticker_muertosskull.printname"] = "Диа-де-Лос-Муэртос-Калавера"
+L["sticker_muertosskull.compactname"] = "Лос-Муэртос"
+L["sticker_muertosskull.description"] = [[«Думаю, это будет мой окончательный вариант, если никто не предложит мне изменить его, название: "Диа-де-Лос-Муэртос-Калавера" описание: не могу придумать, что-то про день мёртвых черепа мексики бла-бла-бла»
+Автор: rooneyviz.
+
+Наклейка входит в состав базы ARC9.]]
+
+L["sticker_bandaid.printname"] = "Лейкопластырь"
+L["sticker_bandaid.compactname"] = "Лейкопластырь"
+L["sticker_bandaid.description"] = [[Приложите его к своему бо-бо и поцелуйте его.
+Автор: Itzal.
+
+Наклейка входит в состав базы ARC9.]]
+
+L["sticker_peacedove.printname"] = "Мир через высшую огневую мощь"
+L["sticker_peacedove.compactname"] = "Высшая огневая мощь"
+L["sticker_peacedove.description"] = [[Голубь приносит мир, но пушка приносит мир благодаря превосходству в огневой мощи.
+Автор: Xipil.
+
+Наклейка входит в состав базы ARC9.]]
+
+L["sticker_borntomodify.printname"] = "Рождённый изменять"
+L["sticker_borntomodify.compactname"] = "Изменятель"
+L["sticker_borntomodify.description"] = [[Ещё один прицел - ещё другой прицел - гранатомёт, то тут, то там...
+Автор: Duck.
+
+Наклейка входит в состав базы ARC9.]]
+
+L["sticker_cad.printname"] = "хам"
+L["sticker_cad.compactname"] = "хам"
+L["sticker_cad.description"] = [[хам
+Автор: Opt1ca.
+
+Наклейка входит в состав базы ARC9.]]
+
+L["sticker_fumo.printname"] = "Фумо"
+L["sticker_fumo.compactname"] = "Фумо"
+L["sticker_fumo.description"] = [[Н-не смотри на меня так, Райфу-кун...
+Автор: Itzal.
+
+Наклейка входит в состав базы ARC9.]]
+
+L["sticker_hotlead.printname"] = "Hot Lead"
+L["sticker_hotlead.compactname"] = "Hot Lead"
+L["sticker_hotlead.description"] = [[Careful! It's hot!
+Автор: Arqu.
+
+Наклейка входит в состав базы ARC9.]]
+
+L["sticker_steamhappy.printname"] = ":steamhappy:"
+L["sticker_steamhappy.compactname"] = ":steamhappy:"
+L["sticker_steamhappy.description"] = [[Счастье от этого
+Автор: Steam.
+
+Наклейка входит в состав базы ARC9.]]
+
+L["sticker_amidstus.printname"] = "Amidst Us"
+L["sticker_amidstus.compactname"] = "Amidst Us"
+L["sticker_amidstus.description"] = [[Зачем довольствоваться оригиналом, если можно купить дешёвую подделку?!
+Автор: Optica.
+
+Наклейка входит в состав базы ARC9.]]
+
+L["sticker_arcglory.printname"] = "Слава!"
+L["sticker_arcglory.compactname"] = "Слава!"
+L["sticker_arcglory.description"] = [[Потребовалось девять клеток мозга, чтобы набросать это
+Автор: Optica.
+
+Наклейка входит в состав базы ARC9.]]
+
+L["sticker_wp.printname"] = "Мир во всём мире!"
+L["sticker_wp.compactname"] = "Мир во всём мире!"
+L["sticker_wp.description"] = [[Заблуждение!
+Автор: Fidget.
+
+Наклейка входит в состав базы ARC9.]]
+
+L["sticker_tonkr.printname"] = "Отправить его"
+L["sticker_tonkr.compactname"] = "Отправить его"
+L["sticker_tonkr.description"] = [[Да, он такой тяжёлый. Да, он такой мощный.
+Автор: CyloWalker.
+
+Наклейка входит в состав базы ARC9.]]
+
+L["sticker_s6amblem.printname"] = "Секция 6"
+L["sticker_s6amblem.compactname"] = "Секция 6"
+L["sticker_s6amblem.description"] = [[Дотянуться до звёзд. Вперёд к славе!
+Автор: CyloWalker.
+
+Наклейка входит в состав базы ARC9.]]
+
+L["sticker_babies.printname"] = "Опасность для ребёнка"
+L["sticker_babies.compactname"] = "Опасность для ребёнка"
+L["sticker_babies.description"] = [[Дети в возрасте до трёх лет должны находиться вдали от этого... огнестрельного оружия.
+Автор: Itzal.
+
+Наклейка входит в состав базы ARC9.]]
+
+L["sticker_drawberf.printname"] = "Берф"
+L["sticker_drawberf.compactname"] = "Берф"
+L["sticker_drawberf.description"] = [[Рисунок Берфа.
+Автор: The Villain.
+
+Наклейка входит в состав базы ARC9.]]
+
+L["sticker_arcbird.printname"] = "Арктическая птица"
+L["sticker_arcbird.compactname"] = "Арктическая птица"
+L["sticker_arcbird.description"] = [[Арктическая птица.
+Автор: Nori/Chewable.
+
+Наклейка входит в состав базы ARC9.]]
