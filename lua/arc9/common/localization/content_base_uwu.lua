9cffff57bdcb0346955d6004dd157ee4cf411cd7
Darsu <95997959+dar-su@users.noreply.github.com>
2023-07-27 23:53:14 +0700

    Create content_base_uwu.lua

---
+++ lua/arc9/common/localization/content_base_uwu.lua
@@ -0,0 +1,273 @@
+L = {}
+
+-- Flat Camo
+L["camo_arc9_base_flat_od.printname"] = "Owiwe Dwab"
+L["camo_arc9_base_flat_od.compactname"] = "OD"
+L["camo_arc9_base_flat_od.description"] = "Stwandawt Owiwe Dwab camowowage"
+
+L["camo_arc9_base_flat_deserttan.printname"] = "Desewt Tan"
+L["camo_arc9_base_flat_deserttan.compactname"] = "Tan"
+L["camo_arc9_base_flat_deserttan.description"] = "Stwandawt Desewt Tan camowowage"
+
+L["camo_arc9_base_flat_arctic.printname"] = "Awktik White"
+L["camo_arc9_base_flat_arctic.compactname"] = "Awktik"
+L["camo_arc9_base_flat_arctic.description"] = "Stwandawt Awktik Wwite camowowage"
+
+L["camo_arc9_base_flat_black.printname"] = "Factowy Bwack"
+L["camo_arc9_base_flat_black.compactname"] = "Bwack"
+L["camo_arc9_base_flat_black.description"] = "Stwandawt Factowy Isuwe Bwack."
+
+L["camo_arc9_base_flat_urbangray.printname"] = "UwUrban Gray"
+L["camo_arc9_base_flat_urbangray.compactname"] = "UwUrban"
+L["camo_arc9_base_flat_urbangray.description"] = "Stwandawt UwUrban Gway camowowage"
+
+L["camo_arc9_base_flat_forestgreen.printname"] = "Fowest Gwin"
+L["camo_arc9_base_flat_forestgreen.compactname"] = "Fowest"
+L["camo_arc9_base_flat_forestgreen.description"] = "Stwandawt Forest Gwin camowowage"
+
+L["camo_arc9_base_flat_navy.printname"] = "Nawuwu Bwue"
+L["camo_arc9_base_flat_navy.compactname"] = "Nawuwu"
+L["camo_arc9_base_flat_navy.description"] = "Stwandawt Nawuwu Bwue cowow"
+
+L["camo_arc9_base_flat_mountbattenpink.printname"] = "Mouwuntbaten Pwink"
+L["camo_arc9_base_flat_mountbattenpink.compactname"] = "MP"
+L["camo_arc9_base_flat_mountbattenpink.description"] = "Stwandawt Mountbatten Pwink camowowage\nShowockingly ewwectiwe in desewt cowondion"
+
+L["camo_arc9_base_flat_fde.printname"] = "Fwat Dawk Eawh"
+L["camo_arc9_base_flat_fde.compactname"] = "FDE >w<"
+L["camo_arc9_base_flat_fde.description"] = "Stwandawt Fwat Dawk Eawh camowowage"
+
+-- Flat Colour
+L["camo_arc9_base_flat_red.printname"] = "Wed"
+L["camo_arc9_base_flat_red.compactname"] = "Wed"
+L["camo_arc9_base_flat_red.description"] = "Bawasic wed cowow"
+
+L["camo_arc9_base_flat_orange.printname"] = "OwOnge"
+L["camo_arc9_base_flat_orange.compactname"] = "OwOnge"
+L["camo_arc9_base_flat_orange.description"] = "Bawasic OwOnge cowow"
+
+L["camo_arc9_base_flat_yellow.printname"] = "Yewoww"
+L["camo_arc9_base_flat_yellow.compactname"] = "Yewwoww"
+L["camo_arc9_base_flat_yellow.description"] = "Bawasic yewwoww cowow"
+
+L["camo_arc9_base_flat_green.printname"] = "Gwin"
+L["camo_arc9_base_flat_green.compactname"] = "Gwin"
+L["camo_arc9_base_flat_green.description"] = "Bawasic gween cowow"
+
+L["camo_arc9_base_flat_blue.printname"] = "Bwue"
+L["camo_arc9_base_flat_blue.compactname"] = "Bwue"
+L["camo_arc9_base_flat_blue.description"] = "Bawasic bwue cowow"
+
+L["camo_arc9_base_flat_purple.printname"] = "Puwpwe"
+L["camo_arc9_base_flat_purple.compactname"] = "Puwpwe"
+L["camo_arc9_base_flat_purple.description"] = "Bawasic puwpwe cowow"
+
+L["camo_arc9_base_flat_pink.printname"] = "Pwink"
+L["camo_arc9_base_flat_pink.compactname"] = "Pwink"
+L["camo_arc9_base_flat_pink.description"] = "Ewye seawingwy bwihgt pwink cowow"
+
+L["camo_arc9_base_flat_arc9orange.printname"] = "AWC9 Owange"
+L["camo_arc9_base_flat_arc9orange.compactname"] = "AWC9 Owange"
+L["camo_arc9_base_flat_arc9orange.description"] = "De pewwekt shawade of owange fow that cUwUte witle AWC9 Wogo stwickew"
+
+-- Charms
+L["charm_gs_killcounter.printname"] = "Kiww Counter"
+L["charm_gs_killcounter.compactname"] = "Kiww!!!"
+L["charm_gs_killcounter.description"] = "Dewice for twacking ur owoweapon kiww stwats"
+
+L["charm_gs_clock.printname"] = "Bawasic Cwock"
+L["charm_gs_clock.compactname"] = "CWOCK"
+L["charm_gs_clock.description"] = "Dewice that displays de real-world time."
+
+L["charm_gs_sticker.printname"] = "Stickew Pawnel"
+L["charm_gs_sticker.compactname"] = "STICKEW"
+L["charm_gs_sticker.description"] = "Dewice that awows a custom stickew to be aplied onto its scween."
+
+-- Stickers, Base
+L["sticker_spray.printname"] = "Playew Spway"
+L["sticker_spray.compactname"] = "Spway"
+L["sticker_spray.description"] = [[Youw own spway as a stickew!
+You will only be able to see youw own and not ones fwom odew playews.
+
+stikew incwuded in de AWC9 base]]
+
+L["sticker_arc9.printname"] = "AWC9 Logo (Low Vis.)"
+L["sticker_arc9.compactname"] = "AWC9"
+L["sticker_arc9.description"] = [[Dawk vewsion of de AWC9 logo.
+desiwgn bwy de Villain.
+
+stikew incwuded in de AWC9 base]]
+
+L["sticker_arc9_lowvis.printname"] = "AWC9 Wogo"
+L["sticker_arc9_lowvis.compactname"] = "AWC9"
+L["sticker_arc9_lowvis.description"] = [[Hewes to you.
+desiwgn bwy de Villain.
+
+stikew incwuded in de AWC9 base]]
+
+L["sticker_a9k.printname"] = "AwCTICTHWON 9.000 WEAPON BASE"
+L["sticker_a9k.compactname"] = "Aw9K"
+L["sticker_a9k.description"] = [[dis is wat "awc9" actuwuawwy mewans
+
+stikew incwuded in de AWC9 base]]
+
+L["sticker_a9cylo.printname"] = "AWC9 Concept logo by CyloWalkew"
+L["sticker_a9cylo.compactname"] = "A9 Cylo"
+L["sticker_a9cylo.description"] = [[One of de fiwst concepts fow AWC9 logo/icon by CyloWalkew (hes cool!)
+
+stikew incwuded in de AWC9 base]]
+
+L["sticker_sus9.printname"] = "Sus 9"
+L["sticker_sus9.compactname"] = "Sus 9"
+L["sticker_sus9.description"] = [[de impostow could be anywhewe among us...
+desiwgn bwy Darsuw.
+
+stikew incwuded in de AWC9 base]]
+
+-- Stickers, Com. Wave 1
+L["sticker_alien_lean.printname"] = "Wegawize Alien Wean"
+L["sticker_alien_lean.compactname"] = "Awien wean"
+L["sticker_alien_lean.description"] = [[Hop dow to Agawtha wit de wite boys fow a cup of awien wean and... odew things.
+desiwgn bwy Opt1ca.
+
+stikew incwuded in de AWC9 base]]
+
+L["sticker_tomatocat.printname"] = "Towomato kity"
+L["sticker_tomatocat.compactname"] = "Towomato kity"
+L["sticker_tomatocat.description"] = [[Sometimes peple ask whedew tomatoes awe fwuits ow wegetwables... dis one migt be a touwughew quwestion.
+desiwgn bwy Endew2Point0.
+
+stikew incwuded in de AWC9 base]]
+
+L["sticker_actionbird.printname"] = "Action biwb"
+L["sticker_actionbird.compactname"] = "Action biwb"
+L["sticker_actionbird.description"] = [[dis is wat Awktik wooks wike in weal wife.
+desiwgn bwy CyloWalkew.
+
+stikew incwuded in de AWC9 base]]
+
+L["sticker_warcrimes.printname"] = "Waw Cwimes"
+L["sticker_warcrimes.compactname"] = "Waw Cwimes"
+L["sticker_warcrimes.description"] = [[Ur awweady doing dem, so why not showt it out woud? Whos gonna stop you  de UwUN?
+desiwgn bwy Wimuchii. Made using Pwoject Sekai stickew base.
+
+stikew incwuded in de AWC9 base]]
+
+L["sticker_sillycat.printname"] = "Siwwy kity"
+L["sticker_sillycat.compactname"] = "Siwwy kity"
+L["sticker_sillycat.description"] = [[imsillyimsillyimsillyimsillyimsillyimsillyimsillyimsilly
+desiwgn bwy Stan_Jacobs.
+
+stikew incwuded in de AWC9 base]]
+
+L["sticker_muertosskull.printname"] = "Dia De Los Muewtos Calavewa"
+L["sticker_muertosskull.compactname"] = "Los Muewtos"
+L["sticker_muertosskull.description"] = [["dis showuld be my winal wewsion i think unless anywone giwes me sugestions to change it de name: "Dia de los muewtos calavewa" de descwiption i cant think of one something about day of de dewad skull mexico bwah bwah bleh"
+desiwgn bwy wooneyviz.
+
+stikew incwuded in de AWC9 base]]
+
+L["sticker_bandaid.printname"] = "Band-Aid"
+L["sticker_bandaid.compactname"] = "Band-Aid"
+L["sticker_bandaid.description"] = [[Puwut it on youw booboo and kiss it aww bettew
+desiwgn bwy Itzal.
+
+stikew incwuded in de AWC9 base]]
+
+L["sticker_peacedove.printname"] = "Peace Thwough Supewiow Fiwepowew"
+L["sticker_peacedove.compactname"] = "PTSF"
+L["sticker_peacedove.description"] = [[de dove bwings peace but de gun bwings peace thwough supewiow fiwepowew
+desiwgn bwy Xipil.
+
+stikew incwuded in de AWC9 base]]
+
+L["sticker_borntomodify.printname"] = "Bown To Modiwy"
+L["sticker_borntomodify.compactname"] = "BTM"
+L["sticker_borntomodify.description"] = [[Anodew siht - anodew scope - a gwenade launchew uwu, hewe and dewe... owo
+desiwgn bwy Duck.
+
+stikew incwuded in de AWC9 base]]
+
+L["sticker_cad.printname"] = "cad"
+L["sticker_cad.compactname"] = "cad"
+L["sticker_cad.description"] = [[cad
+desiwgn bwy Opt1ca.
+
+stikew incwuded in de AWC9 base]]
+
+L["sticker_fumo.printname"] = "Fumo"
+L["sticker_fumo.compactname"] = "Fumo"
+L["sticker_fumo.description"] = [[D-don't look at me like that, waifu-kun...
+desiwgn bwy Itzal.
+
+stikew incwuded in de AWC9 base]]
+
+L["sticker_hotlead.printname"] = "Hot Lead"
+L["sticker_hotlead.compactname"] = "Hot Lead"
+L["sticker_hotlead.description"] = [[Caweful! It's hot!
+desiwgn bwy Awqu.
+
+stikew incwuded in de AWC9 base]]
+
+L["sticker_steamhappy.printname"] = ":steamhappy:"
+L["sticker_steamhappy.compactname"] = ":steamhappy:"
+L["sticker_steamhappy.description"] = [[Happy abowut it
+Design fwom Steam.
+
+stikew incwuded in de AWC9 base]]
+
+L["sticker_amidstus.printname"] = "Amidstus"
+L["sticker_amidstus.compactname"] = "Amidstus"
+L["sticker_amidstus.description"] = [[y setwe fow de owiginal when you could have a cheap knockoff?!
+desiwgn fwom OwOptica.
+
+stikew incwuded in de AWC9 base]]
+
+L["sticker_arcglory.printname"] = "Glowy!"
+L["sticker_arcglory.compactname"] = "Glorwy!"
+L["sticker_arcglory.description"] = [[Took nine bwain cells to scwibble dis
+desiwgn fwom OwOptica.
+
+stikew incwuded in de AWC9 base]]
+
+L["sticker_wp.printname"] = "Wowld Peece!"
+L["sticker_wp.compactname"] = "Wowld Piss!"
+L["sticker_wp.description"] = [[A delusion!
+desiwgn fwom Fidget.
+
+stikew incwuded in de AWC9 base]]
+
+L["sticker_tonkr.printname"] = "Swend It"
+L["sticker_tonkr.compactname"] = "Sewnd It"
+L["sticker_tonkr.description"] = [[Yewah its that heawy. Yweah its that powewful.
+desiwgn fwom CyloWalkew.
+
+stikew incwuded in de AWC9 base]]
+
+L["sticker_s6amblem.printname"] = "Swection 6"
+L["sticker_s6amblem.compactname"] = "Sewction 6"
+L["sticker_s6amblem.description"] = [[Weach fow de staws. Fowwawd to Glowy!
+desiwgn fwom CyloWalkew.
+
+stikew incwuded in de AWC9 base]]
+
+L["sticker_babies.printname"] = "Baby Hazawd"
+L["sticker_babies.compactname"] = "Baby Hazawd"
+L["sticker_babies.description"] = [[Childwen ages 0-3 should be kept away fwom dis.. fiweawm
+desiwgn fwom Itzal.
+
+stikew incwuded in de AWC9 base]]
+
+L["sticker_drawberf.printname"] = "Bewf"
+L["sticker_drawberf.compactname"] = "Beww"
+L["sticker_drawberf.description"] = [[Dwaw Bewf.
+desiwgn bwy de Villain.
+
+stikew incwuded in de AWC9 base]]
+
+L["sticker_arcbird.printname"] = "Awc Biwb"
+L["sticker_arcbird.compactname"] = "Awc Biwb"
+L["sticker_arcbird.description"] = [[Awc Biwb.
+desiwgn bwy Nowi/Chewable.
+
+stikew incwuded in de AWC9 base]]
