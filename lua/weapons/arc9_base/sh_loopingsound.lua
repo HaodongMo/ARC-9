f5eda85d271ed0fd96712f81c51799ca44a7d5a9
TheOnly8Z <888888zombies@gmail.com>
2023-10-25 21:05:13 -0700

    Replace indoor detection with UC innyouty

--- lua/weapons/arc9_base/sh_loopingsound.lua
+++ lua/weapons/arc9_base/sh_loopingsound.lua
@@ -85,6 +85,7 @@ end
 SWEP.IndoorTick = 0
 SWEP.IsIndoors = 0
 
+--[[]
 local dirs = {
     Angle(-90, 90, 0), -- Up            angled by 15 degrees + diagonal direction
     Angle(-75, 135, 0), -- Up right
@@ -95,6 +96,35 @@ local dirs = {
     Angle(-15, 0, 0), -- side
     Angle(-15, 120, 0), -- side
     Angle(-15, 240, 0), -- side
+}
+]]
+
+local traces = {
+    {
+        Distance = Vector(0, 0, 1024),
+        Influence = 0,
+    }, -- Up
+    {
+        Distance = Vector(0, 768, 768),
+        Influence = 1,
+    }, -- Up Forward
+    {
+        Distance = Vector(0, -768, 768),
+        Influence = 1,
+    }, -- Up Back
+    {
+        Distance = Vector(0, 768, 0),
+        Influence = 0.5,
+    }, -- Forward
+    {
+        Distance = Vector(768, 768, 0),
+        Influence = 0.5,
+    }, -- Right
+    {
+        Distance = Vector(-768, 768, 0),
+        Influence = 0.5,
+    }, -- Left
+
 }
 
 
@@ -111,9 +141,39 @@ function SWEP:GetIndoor()
     if !self.ShootSoundIndoor and !self.DistantShootSoundIndoor and !self.ShootSoundSilencedIndoor and !self.DistantShootSoundSilencedIndoor then return 0 end -- non realism guns!!!
 
     if self.IndoorTick == UnPredictedCurTime() then return self.IsIndoors end
-
     self.IndoorTick = UnPredictedCurTime()
 
+    ------------------------- the one Fesiug wrote for UC (smooth)
+    local vol = 0
+    local wo = self:GetOwner()
+    if !IsValid(wo) then return end
+    local wop = wo:EyePos()
+    local woa = Angle(0, wo:EyeAngles().y, 0)
+    local t_influ = 0
+
+    for _, tin in ipairs(traces) do
+        traceTable.start = wop
+        offset = Vector()
+        offset = offset + (tin.Distance.x * woa:Right())
+        offset = offset + (tin.Distance.y * woa:Forward())
+        offset = offset + (tin.Distance.z * woa:Up())
+        traceTable.endpos = wop + offset
+        traceTable.filter = wo
+        t_influ = t_influ + (tin.Influence or 1)
+        local result = util.TraceLine(traceTable)
+        if GetConVar("developer"):GetInt() > 2 then
+            debugoverlay.Line(wop - (vector_up * 4), result.HitPos - (vector_up * 4), 1, Color((_ / 4) * 255, 0, (1 - (_ / 4)) * 255))
+            debugoverlay.Text(result.HitPos - (vector_up * 4), math.Round((result.HitSky and 1 or result.Fraction) * 100) .. "%", 1)
+        end
+        vol = vol + (result.HitSky and 1 or result.Fraction) * tin.Influence
+    end
+
+    self.IsIndoors = 1 - vol / t_influ
+
+    return self.IsIndoors
+
+    ------------------------- old one (bad) (not good)
+    --[[]
     local isindoors = 0
 
     local hits = 0
@@ -156,8 +216,6 @@ function SWEP:GetIndoor()
     end
 
     isindoors = math.min(isindoors, 1)
-
     self.IsIndoors = isindoors
-
-    return isindoors
+    ]]
 end
