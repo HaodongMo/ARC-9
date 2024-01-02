116b3448e3d5fc95c56a87221c7f9342f594e865
MokaAkashiya85 <ThaERROR404@gmail.com>
2024-01-02 18:14:53 +0100

    Update base_sv-se.lua
    Fixed some fuckery that occurred on the Swedish localization file (What even?); Also fixed untranslated lines

--- lua/arc9/common/localization/base_sv-se.lua
+++ lua/arc9/common/localization/base_sv-se.lua
@@ -251,7 +251,7 @@ L["autostat.secondary._beforephrase"] = false
 L["autostat.secondary._divider"] = " "
 -- space between the secondary phrase from the stat phrase. set true to use empty string
 
--- L["autostat.secondary.blindfire"] = "while Blind-Firing"
+-- L["autostat.secondary.blindfire"] = "under blind skjutning"
 L["autostat.secondary.bipod"] = "med Skjutstöd"
 L["autostat.secondary.crouch"] = "medans Hukad"
 L["autostat.secondary.empty"] = "på Sista skottet i magasinet"
@@ -349,9 +349,9 @@ L["settings.title"] = "ARC9-Inställningar"
 L["settings.desc"] = "Beskrivning"
 
 L["settings.default_convar"] = "Standard värde"
--- L["settings.convar_server"] = "Server variable"
+L["settings.convar_server"] = "Servervariabel"
 
--- L["settings.convar_client"] = "Client variable"
+L["settings.convar_client"] = "Client-variabel"
 
 
 ////////////////////// General
@@ -375,8 +375,8 @@ L["settings.reset"] = "Återställ!"
 ////////////////////// Performance
 L["settings.tabname.performance"] = "Prestanda"
 
--- L["settings.optics.cheapscopes.title"] = "Cheap Scopes"
--- L["settings.optics.cheapscopes.desc"] = "A cheap RT scope implementation by zooming your whole view to aim, instead of rendering world again in a scope. Significantly increases performance on large maps."
+L["settings.optics.cheapscopes.title"] = "Billiga Sikten"
+L["settings.optics.cheapscopes.desc"] = "En billig RT-sikte genomförande genom att zooma in hela din vy när du siktar istället för att framställa världen igen i siktet. Ökar drastiskt prestandan på större kartor."
 
 L["settings.performance.important"] = "Viktigt"
 L["settings.cheapscopes.title"] = "Billiga Sikten"
@@ -493,10 +493,8 @@ L["settings.hud_game.hud_nohints.title"] = "Avaktivera Tips"
 L["settings.hud_game.hud_nohints.desc"] = "Avaktiverar tips helt och hållet. Dem kommer inte visas upp i 3D eller 2D gränssnittet."
 L["settings.hud_game.hud_keephints.title"] = "Behåll Tips"
 L["settings.hud_game.hud_keephints.desc"] = "Visa ARC9-kontroll tipsen på toppen av gränssnitt panelen."
--- L["settings.hud_game.hud_force_disable.title"] = "Force Disable HUD"
-
--- L["settings.hud_game.hud_force_disable.desc"] = "Disable ARC9 HUD for all players. Does not affect hints."
-
+L["settings.hud_game.hud_force_disable.title"] = "Tvinga Av ARC9 Gränssnitt"
+L["settings.hud_game.hud_force_disable.desc"] = "Stänger av ARC9 Gränssnittet till alla spelare. Gäller ej för knapptips."
 
 L["settings.hud_game.killfeed"] = "Killfeed"
 L["settings.hud_game.killfeed_enable.title"] = "Killfeed-Ikoner"
@@ -518,8 +516,8 @@ L["settings.hud_game.centerhint"] = "Verktygstips"
 L["settings.hud_game.centerhint_reload.title"] = "Visa Omladdning Tips"
 L["settings.hud_game.centerhint_reload.desc"] = "Visar ett omladdning tips när ammunition är lågt."
 L["settings.hud_game.centerhint_reload_percent.title"] = "Magasinprocent"
-L["settings.hud_game.centerhint_reload_percent.desc"] = "Visar omladdningstipset när det nuvarande magasinet är vid detta procentvärde."L["settings.hud_game.centerhint_bipod.title"] -- L["settings.hud_game.centerhint_bipod.title"] -- L["settings.hud_game.centerhint_bipod.title"] -- L["settings.hud_game.centerhint_bipod.title"] -- L["settings.hud_game.centerhint_bipod.title"] -- L["settings.hud_game.centerhint_bipod.title"] -- L["settings.hud_game.centerhint_bipod.title"] -- L["settings.hud_game.centerhint_bipod.title"] = "Display Toggle Bipod"
-
+L["settings.hud_game.centerhint_reload_percent.desc"] = "Visar omladdningstipset när det nuvarande magasinet är vid detta procentvärde."
+L["settings.hud_game.centerhint_bipod.title"] = "Visa Växla Skjutstöd"
 L["settings.hud_game.centerhint_bipod.desc"] = "Visa ett tips när skjutstödet kan växlas."
 
 ////////////////////// NPCs
