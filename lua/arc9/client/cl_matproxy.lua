5c2d9feecb3c7cbbff2ac26d9e69cefcd741e3e7
May Lian <twilight_sparkle13@hotmail.com>
2024-01-05 14:38:52 -0300

    New RT lens textures/shaders

--- lua/arc9/client/cl_matproxy.lua
+++ lua/arc9/client/cl_matproxy.lua
@@ -109,41 +109,38 @@ matproxy.Add({
    end
 })
 
-
-local envmaptint = "$envmaptint"
-local envmap = "$envmap"
 local lastPos = Vector()
 local lastValue = 0
-
+local lerp = Lerp
 matproxy.Add( {
-    name = "Arc9EnvMapTint",
-    
-    init = function(self, mat, values)
-        local color = {1, 1, 1} 
+	name = "Arc9EnvMapTint",
+	
+	init = function(self, mat, values)
+		local color = {1, 1, 1} 
 
-        if (values.color != nil) then
-            color = string.Explode(" ", string.Replace(string.Replace(values.color, "[", ""), "]", ""))
-        end
+		if (values.color != nil) then
+			color = string.Explode(" ", string.Replace(string.Replace(values.color, "[", ""), "]", ""))
+		end
 
-        self.min = values.min || 0
-        self.max = values.max || 1
-        self.color = Vector(color[1], color[2], color[3])
-        mat:SetTexture(envmap, values.envmap || "arc9/shared/envmaps/specularity_50")
-    end,
+		self.min = values.min || 0
+		self.max = values.max || 1
+		self.color = Vector(color[1], color[2], color[3])
+		mat:SetTexture("$envmap", values.envmap || "arc9/shared/envmaps/specularity_50")
+	end,
 
-    bind = function(self, mat, ent)
-        if (!IsValid(ent)) then return end
+	bind = function(self, mat, ent)
+		if (!IsValid(ent)) then return end
 
-        if (!lastPos:IsEqualTol(ent:GetPos(), 1)) then
-            local c = render.GetLightColor(ent:GetPos())
-            lastValue = (c.x * 0.2126) + (c.y * 0.7152) + (c.z * 0.0722)
-            lastValue = math.min(lastValue * 2, 1)
-            lastPos = ent:GetPos()
-        end
+		if (!lastPos:IsEqualTol(ent:GetPos(), 1)) then
+			local c = render.GetLightColor(ent:GetPos())
+			lastValue = (c.x * 0.2126) + (c.y * 0.7152) + (c.z * 0.0722)
+			lastValue = math.min(lastValue * 2, 1)
+			lastPos = ent:GetPos()
+		end
 
-        ent.m_Arc9EnvMapTint = Lerp(10 * FrameTime(), ent.m_Arc9EnvMapTint || 0, lastValue)
-        mat:SetVector(envmaptint, self.color * Lerp(ent.m_Arc9EnvMapTint, self.min, self.max))
-    end
+		ent.m_Arc9EnvMapTint = lerp(10 * RealFrameTime(), ent.m_Arc9EnvMapTint || 0, lastValue)
+		mat:SetVector("$envmaptint", self.color * lerp(ent.m_Arc9EnvMapTint, self.min, self.max))
+	end
 })
 
 ----- Add this to your VMT to fix garbage/shitty reflection
