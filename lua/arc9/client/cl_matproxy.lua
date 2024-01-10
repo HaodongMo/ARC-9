bf8d111ceba62e16f9876ae8389549c9b7e08cfa
Darsu <95997959+dar-su@users.noreply.github.com>
2024-01-11 06:13:05 +0700

    reverting rt lense changes, instant flicker not good
    aalso higher envmap values

--- lua/arc9/client/cl_matproxy.lua
+++ lua/arc9/client/cl_matproxy.lua
@@ -89,47 +89,24 @@ matproxy.Add({
     end
 })
 
--- matproxy.Add({
-    -- name = "arc9_scope_alpha",
-    -- init = function(self, mat, values)
-        -- self.ResultTo = values.resultvar
-    -- end,
-    -- bind = function(self, mat, ent)
-        -- local ply = LocalPlayer()
-
-        -- if IsValid(ply) then
-            -- local weapon = ply:GetActiveWeapon()
-
-            -- if IsValid(weapon) and weapon.ARC9 then
-                -- local amt = 1 - weapon:GetSightAmount() / 1 
-                -- amt = amt * 0.2
-                -- mat:SetVector(self.ResultTo, Vector(amt*1.1, amt*0.9, amt*1.3))
-            -- end
-        -- end
-   -- end
--- })
-
-local function getWeaponSightAmount()
-  if (IsValid(LocalPlayer():GetActiveWeapon()) && LocalPlayer():GetActiveWeapon().GetSightAmount != nil) then
-    return LocalPlayer():GetActiveWeapon():GetSightAmount()
-  end
-  return 0
-end
-
-matproxy.Add( {
+matproxy.Add({
     name = "arc9_scope_alpha",
-    
     init = function(self, mat, values)
+        self.ResultTo = values.resultvar
     end,
-
     bind = function(self, mat, ent)
-        if (!IsValid(ent)) then return end
+        local ply = LocalPlayer()
 
-    --print(getWeaponSightAmount())
+        if IsValid(ply) then
+            local weapon = ply:GetActiveWeapon()
 
-        mat:SetInt("$cloakpassenabled", 1)
-        mat:SetFloat("$cloakfactor", math.ceil(getWeaponSightAmount()))
-    end
+            if IsValid(weapon) and weapon.ARC9 then
+                local amt = 1 - weapon:GetSightAmount() / 1 
+                amt = amt * 0.2
+                mat:SetVector(self.ResultTo, Vector(amt*2.2, amt*1.8, amt*2.6))
+            end
+        end
+   end
 })
 
 local lastPos = Vector()
