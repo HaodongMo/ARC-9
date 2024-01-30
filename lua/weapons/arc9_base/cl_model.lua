8cffd2b2e1a060beb2967b7801179379582a9c8b
Darsu <95997959+dar-su@users.noreply.github.com>
2024-01-31 06:11:14 +0700

    crazy SWEP.StickersNoNocull

--- lua/weapons/arc9_base/cl_model.lua
+++ lua/weapons/arc9_base/cl_model.lua
@@ -532,8 +532,15 @@ function SWEP:SetupModel(wm, lod, cm)
                 end
             end
             stickermodel:SetParent(stickerparent)
+            local stickermat = atttbl.StickerMaterial
 
-            stickermodel:SetMaterial(atttbl.StickerMaterial)
+            if self.StickersNoNocull then
+                local fakestickwithoutnocull = Material(stickermat)
+                fakestickwithoutnocull:SetInt("$flags", bit.band(fakestickwithoutnocull:GetInt("$flags"), bit.bnot(8192)))
+                fakestickwithoutnocull:Recompute()
+            end
+            
+            stickermodel:SetMaterial(stickermat)
 
             local tbl = {
                 Model = stickermodel,
