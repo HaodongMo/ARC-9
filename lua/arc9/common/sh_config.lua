0f7dbef066b86f87082245bce17516c9d7a56d71
pwsf2 <131566207+pwsf2@users.noreply.github.com>
2024-06-01 00:04:27 -0400

    Add files via upload

---
+++ lua/arc9/common/sh_config.lua
@@ -0,0 +1,6 @@
+// So servers don't have to mess with ConVars
+
+ARC9.NoHUD = false
+ARC9.NoTPIK = false
+ARC9.NoArmorPiercing = false
+ARC9.NoBodyPartsDamageMults = false
---
+++ lua/arc9/common/sh_truenames.lua
@@ -0,0 +1,15 @@
+local truenames_enforced = GetConVar("arc9_truenames_enforced")
+local truenames_default = GetConVar("arc9_truenames_default")
+local truenames_preference = CLIENT and GetConVar("arc9_truenames")
+
+--[[
+    If called from the server or sv is true, returns the server truenames mode.
+    If called from the client, returns the client preference if exists and is not enforced.
+]]
+function ARC9:UseTrueNames(sv)
+    return truenames_default:GetBool()
+    -- if SERVER or sv or truenames_enforced:GetBool() or truenames_preference:GetInt() == 2 then
+    --     return truenames_default:GetBool()
+    -- end
+    -- return truenames_preference:GetBool()
+end
