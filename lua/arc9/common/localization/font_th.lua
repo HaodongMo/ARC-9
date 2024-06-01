3e9ea7467314a7f377dffb46f4740b0b61e2b987
pwsf2 <131566207+pwsf2@users.noreply.github.com>
2024-06-01 17:10:25 -0400

    Delete lua/arc9/common/localization directory

--- lua/arc9/common/localization/font_th.lua
+++
@@ -1,7 +0,0 @@
-L = {}
-
-/////////////////////////////////////// Font
-//////////////////// If a custom font should be used, rename the string. Useful for Pinyin, Kanji, etc.
-////////// Do not create a font file if your language does not need it.
-
-L["font"] = "Leelawadee UI"
