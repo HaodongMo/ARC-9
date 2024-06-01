a4b2300066d6994e8188c2a4e01af1ec9d0264f2
pwsf2 <131566207+pwsf2@users.noreply.github.com>
2024-06-01 00:03:56 -0400

    Add files via upload

---
+++ lua/arc9/client/cl_font.lua
@@ -0,0 +1,139 @@
+local font_cvar = (game.SinglePlayer() or CLIENT) and GetConVar("arc9_font")
+local fontaddsize_cvar = (game.SinglePlayer() or CLIENT) and GetConVar("arc9_font_addsize")
+
+function ARC9:GetFont()
+    local f = font_cvar and font_cvar:GetString()
+    if !f or f == "" then f = ARC9:GetPhrase("font") or "Venryn Sans" end
+    --if !f or f == "" then f = "Venryn Sans" end
+    return f
+end
+
+function ARC9:GetUnscaledFont()
+    local f = ARC9:GetPhrase("unscaled_font") or "HD44780A00 5x8"
+    return f
+end
+
+local arc9_hud_scale = GetConVar("arc9_hud_scale")
+if !ARC9.ScreenScale then ARC9.ScreenScale = function(size) return size * (ScrW() / 640) * arc9_hud_scale:GetFloat() * 0.9 end end -- idk
+
+local sizes_to_make = {
+    4,
+    6,
+    7,
+    8,
+    9,
+    10,
+    12,
+    16,
+    20,
+    24,
+    32
+}
+
+local unscaled_sizes_to_make = {
+    12,
+    16,
+    24,
+    32,
+    48,
+    64
+}
+
+local function generatefonts()
+    local font = ARC9:GetFont()
+    local unscaled_font = ARC9:GetUnscaledFont()
+    local addsize = fontaddsize_cvar:GetInt() or 0
+
+    for _, i in pairs(sizes_to_make) do
+
+        surface.CreateFont( "ARC9_" .. tostring(i), {
+            font = font,
+            size = ARC9.ScreenScale(i + addsize),
+            weight = i < 16 and 650 or 600,
+            antialias = true,
+            extended = true, -- Required for non-latin fonts
+        } )
+
+        surface.CreateFont( "ARC9_" .. tostring(i) .. "_Slim", {
+            font = font,
+            size = ARC9.ScreenScale(i + addsize),
+            weight = 300,
+            antialias = true,
+            extended = true,
+        } )
+
+        surface.CreateFont( "ARC9_" .. tostring(i) .. "_Glow", {
+            font = font,
+            size = ARC9.ScreenScale(i + addsize),
+            weight = 600,
+            antialias = true,
+            blursize = ARC9.ScreenScale(i * 0.2),
+            extended = true,
+        } )
+
+    end
+
+    for _, i in pairs(unscaled_sizes_to_make) do
+
+        surface.CreateFont( "ARC9_" .. tostring(i) .. "_Unscaled", {
+            font = unscaled_font,
+            size = i,
+            weight = 500,
+            antialias = true,
+            extended = true,
+        } )
+
+        surface.CreateFont( "ARC9_" .. tostring(i) .. "_Unscaled_Glow", {
+            font = unscaled_font,
+            size = i,
+            weight = 500,
+            antialias = true,
+            blursize = i * 0.2,
+            extended = false,
+        } )
+
+        surface.CreateFont( "ARC9_" .. tostring(i) .. "_LCD", {
+            font = "HD44780A00 5x8",
+            size = i,
+            weight = 500,
+            antialias = true,
+            extended = true,
+        } )
+
+        surface.CreateFont( "ARC9_" .. tostring(i) .. "_LCD_Glow", {
+            font = "HD44780A00 5x8",
+            size = i,
+            weight = 500,
+            antialias = true,
+            blursize = i * 0.2,
+            extended = false,
+        } )
+
+    end
+
+end
+
+surface.CreateFont( "ARC9_Deco_8_Unscaled", {
+    font = "Consolas",
+    size = 8,
+    weight = 500,
+    antialias = true,
+    extended = true,
+} )
+
+generatefonts()
+
+timer.Simple(2, generatefonts) -- im sorry but new hud default 0.9 mult doesn't want to work on fonts otherwise
+
+function ARC9.Regen()
+    generatefonts()
+end
+
+concommand.Add("arc9_font_reload", ARC9.Regen)
+
+hook.Add("OnScreenSizeChanged", "ARC9.FontRegen", function(oldWidth, oldHeight)
+    print("Warning: Resolution was changed. If ARC9 fonts are too small/big now, try type  arc9_font_reload  in console ")
+    timer.Simple(5, ARC9.Regen)
+end)
+
+-- cvars.AddChangeCallback("arc9_font", ARC9.Regen, "reload_fonts")
---
+++ lua/arc9/client/cl_move.lua
@@ -0,0 +1,241 @@
+local arc9_lean_direction = nil
+
+local arc9_autoreload = GetConVar("arc9_autoreload")
+local arc9_autolean = GetConVar("arc9_autolean")
+
+ARC9.ReloadAmount = 0
+
+hook.Add("CreateMove", "ARC9_CreateMove", function(cmd)
+    local wpn = LocalPlayer():GetActiveWeapon()
+    local ply = LocalPlayer()
+
+    if !IsValid(wpn) then return end
+    if !wpn.ARC9 then return end
+
+    if (arc9_autoreload:GetBool() or wpn:GetRequestReload()) and
+        !wpn:GetCustomize() and
+        !wpn:GetCustomize() and
+        wpn:CanReload() and
+        cmd:TickCount() % 2 == 0
+    then
+        if wpn:GetUBGL() then
+            if !LocalPlayer():KeyDown(IN_USE) and wpn:Clip2() == 0 and wpn:Ammo2() > 0 then
+                cmd:AddKey(IN_RELOAD)
+            end
+        else
+            if !LocalPlayer():KeyDown(IN_USE) and wpn:Clip1() == 0 and wpn:Ammo1() > 0 then
+                cmd:AddKey(IN_RELOAD)
+            end
+        end
+    end
+
+    if ARC9.KeyPressed_Menu then
+        cmd:AddKey(IN_WEAPON1)
+    end
+
+    if ARC9.KeyPressed_Melee then
+        cmd:AddKey(ARC9.IN_MELEE)
+    end
+
+    if ARC9.KeyPressed_UBGL then
+        cmd:AddKey(ARC9.IN_UBGL)
+    end
+
+    if ARC9.KeyPressed_Inspect then
+        cmd:AddKey(ARC9.IN_INSPECT)
+    end
+
+    if ARC9.KeyPressed_SwitchSights then
+        cmd:AddKey(ARC9.IN_SWITCHSIGHTS)
+    end
+
+    if GetConVar("arc9_cruelty_reload"):GetBool() then
+        local buttons = cmd:GetButtons()
+
+        local shouldreload = false
+
+        // reload like in cruelty squad!
+
+        if bit.band(cmd:GetButtons(), IN_RELOAD) == IN_RELOAD then
+            local mouseY = cmd:GetMouseY()
+
+            if mouseY > 0 then
+                ARC9.ReloadAmount = ARC9.ReloadAmount + (mouseY / ScrH())
+            end
+
+            cmd:SetMouseY(0)
+            if lastviewangles then
+                cmd:SetViewAngles(lastviewangles)
+            end
+
+            buttons = bit.band(buttons, bit.bnot(IN_RELOAD))
+        else
+            lastviewangles = cmd:GetViewAngles()
+        end
+
+        if ARC9.ReloadAmount >= 1 then
+            shouldreload = true
+        end
+
+        // deny reload unless shouldreload is true
+
+        if shouldreload then
+            buttons = bit.bor(buttons, IN_RELOAD)
+        else
+            buttons = bit.band(buttons, bit.bnot(IN_RELOAD))
+
+            if bit.band(cmd:GetButtons(), IN_USE) == IN_USE then
+                buttons = bit.bor(buttons, ARC9.IN_INSPECT)
+            end
+        end
+
+        cmd:SetButtons(buttons)
+    end
+
+    if arc9_autolean:GetBool() then
+        if cmd:KeyDown(IN_ATTACK2) or (wpn:ToggleADS() and arc9_lean_direction != nil and arc9_lean_direction != 0) then
+            if arc9_lean_direction != nil and arc9_lean_direction != 0 then
+                if wpn:ToggleADS() then
+                    local eyepos = ply:EyePos()
+                    local forward = ply:EyeAngles():Forward()
+
+                    local covertrace = util.TraceHull({
+                        start = eyepos,
+                        endpos = eyepos + forward * 32,
+                        filter = ply,
+                        mins = Vector(-1, -1, -1) * 4,
+                        maxs = Vector(1, 1, 1) * 4
+                    })
+
+                    if !covertrace.Hit then
+                        arc9_lean_direction = 0
+                        return
+                    end
+                end
+
+                if arc9_lean_direction > 0 then
+                    cmd:AddKey(IN_ALT2)
+                elseif arc9_lean_direction < 0 then
+                    cmd:AddKey(IN_ALT1)
+                end
+            elseif arc9_lean_direction == nil and cmd:KeyDown(IN_ATTACK2) then
+                local eyepos = ply:EyePos()
+                local right = ply:EyeAngles():Right()
+                local forward = ply:EyeAngles():Forward()
+
+                local covertrace = util.TraceHull({
+                    start = eyepos,
+                    endpos = eyepos + forward * 32,
+                    filter = ply,
+                    mins = Vector(-1, -1, -1) * 4,
+                    maxs = Vector(1, 1, 1) * 4
+                })
+
+                if covertrace.Hit then
+                    -- See if it's valid to lean left
+
+                    arc9_lean_direction = 0
+
+                    local leftleanamt = 0
+                    local rightleanamt = 0
+
+                    local leftleantrace = util.TraceLine({
+                        start = eyepos,
+                        endpos = eyepos + right * -wpn.MaxLeanOffset,
+                        filter = ply,
+                    })
+
+                    if !leftleantrace.Hit then
+                        -- see if it's possible to lean in this direction
+
+                        local leftleantrace2 = util.TraceLine({
+                            start = leftleantrace.HitPos,
+                            endpos = leftleantrace.HitPos + (forward * 96),
+                            filter = ply,
+                        })
+
+                        if !leftleantrace2.Hit then
+                            local leftleantrace3 = util.TraceLine({
+                                start = leftleantrace2.HitPos,
+                                endpos = leftleantrace2.HitPos + (right * wpn.MaxLeanOffset),
+                                filter = ply,
+                            })
+
+                            leftleanamt = leftleantrace3.Fraction * wpn.MaxLeanOffset
+                        end
+                    end
+
+                    -- See if it's valid to lean right
+
+                    local rightleantrace = util.TraceLine({
+                        start = eyepos,
+                        endpos = eyepos + right * wpn.MaxLeanOffset,
+                        filter = ply,
+                    })
+
+                    if !rightleantrace.Hit then
+                        -- see if it's possible to lean in this direction
+
+                        local rightleantrace2 = util.TraceLine({
+                            start = rightleantrace.HitPos,
+                            endpos = rightleantrace.HitPos + (forward * 96),
+                            filter = ply,
+                        })
+
+                        if !rightleantrace2.Hit then
+                            local rightleantrace3 = util.TraceLine({
+                                start = rightleantrace2.HitPos,
+                                endpos = rightleantrace2.HitPos + (right * -wpn.MaxLeanOffset),
+                                filter = ply,
+                            })
+
+                            rightleanamt = rightleantrace3.Fraction * wpn.MaxLeanOffset
+                        end
+                    end
+
+                    if leftleanamt > rightleanamt then
+                        arc9_lean_direction = -1
+                    elseif rightleanamt > leftleanamt then
+                        arc9_lean_direction = 1
+                    else
+                        arc9_lean_direction = 0
+                    end
+                end
+            end
+        else
+            arc9_lean_direction = nil
+        end
+    end
+end)
+
+local performedAprilFoolsCheck = false
+
+hook.Add("Think", "ARC9_cruelty_think_client_reload", function()
+    if !performedAprilFoolsCheck then
+        -- is it april fools today
+        local date = os.date("*t")
+
+        local day = date.day
+        local month = date.month
+
+        if day == 1 and month == 4 then
+            if !GetConVar("arc9_cruelty_reload_april_fools"):GetBool() then
+                RunConsoleCommand("arc9_cruelty_reload_april_fools", "1")
+                RunConsoleCommand("arc9_cruelty_reload", "1")
+            end
+        else
+            if GetConVar("arc9_cruelty_reload_april_fools"):GetBool() then
+                RunConsoleCommand("arc9_cruelty_reload_april_fools", "0")
+                RunConsoleCommand("arc9_cruelty_reload", "0")
+            end
+        end
+
+        performedAprilFoolsCheck = true
+    end
+
+    if !GetConVar("arc9_cruelty_reload"):GetBool() then return end
+
+    ARC9.ReloadAmount = ARC9.ReloadAmount - (FrameTime() * 2)
+
+    ARC9.ReloadAmount = math.Clamp(ARC9.ReloadAmount, 0, 1.5)
+end)
