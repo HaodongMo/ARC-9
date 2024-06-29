6b41a7834a06e91cc06e91fb74ac28af73d7cc02
grande1900 <jlyric2015@gmail.com>
2024-06-29 00:08:20 -0400

    Forgot to enable recursion on autolocalize.py
    🤦‍♀️

--- .Stuff for developers/autolocalize.py
+++ .Stuff for developers/autolocalize.py
@@ -5,26 +5,23 @@ Exclude = input("Exclude: ")
 import glob
 import os
 
-def makeTemplate():
-    with open(BaseFile, "r", encoding='utf-8') as f:
-        Template = f.readlines()
-        Dictionary = {}
+Dictionary = {}
+
+with open(BaseFile, "r", encoding='utf-8') as f:
+    Template = f.readlines()
     for i in range(len(Template)):
         split = Template[i].split(" = ")
         if Template[i][:2] == "L[":
             Dictionary[split[1].rstrip()] = (split[0])[2:-1]
-    return Dictionary
 
-# print(makeTemplate())
-
-for path in glob.glob(Directory+'*.lua'): # process
-    Template = makeTemplate()
-    if Exclude in path:
+for path in glob.iglob(Directory+'/**/*.lua',recursive = True): # process
+    if len(Exclude) > 0 and Exclude in path:
         print(f"Excluded file {path}.")
         continue
+    print(path)
     with open(path, 'r+') as lua:
         content = lua.read()
-        for key in Template:
+        for key in Dictionary:
             content = content.replace(key,f"ARC9:GetPhrase({Template[key]})")
         lua.seek(0)
         lua.write(content)
