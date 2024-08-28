276384aea1a755c61460468945680d083649eafc
Darsu <95997959+dar-su@users.noreply.github.com>
2024-08-28 10:22:11 +0700

    Peeking sound effect

--- lua/weapons/arc9_base/cl_peek.lua
+++ lua/weapons/arc9_base/cl_peek.lua
@@ -5,6 +5,19 @@ local lastpressed = false
 local arc9_togglepeek_reset = GetConVar("arc9_togglepeek_reset")
 local arc9_togglepeek = GetConVar("arc9_togglepeek")
 
+local soundin = {
+    name = "firemode",
+    sound = "arc9/cloth_5.ogg",
+    channel = ARC9.CHAN_FIDDLE,
+    volume = 0.3,
+}
+local soundout = {
+    name = "firemode",
+    sound = "arc9/cloth_4.ogg",
+    channel = ARC9.CHAN_FIDDLE,
+    volume = 0.3,
+}
+
 function SWEP:ThinkPeek()
 	if !self.dt.InSights then return end
 
@@ -14,10 +27,17 @@ function SWEP:ThinkPeek()
     if arc9_togglepeek:GetBool() then
         if binding and !lastpressed then
             self.Peeking = !self.Peeking
+            
+            self:PlayTranslatedSound(self.Peeking and soundout or soundin)
         end
     else
         self.Peeking = binding
+
+        if binding != lastpressed then
+            self:PlayTranslatedSound(self.Peeking and soundout or soundin)
+        end
     end
+    
 
     lastpressed = binding
 end
