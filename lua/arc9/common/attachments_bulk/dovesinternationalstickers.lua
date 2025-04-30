73f274cf5ac3c1764f3939cbcc471a9f43a4a7cc
Aerosun <83192028+AerosunST@users.noreply.github.com>
2025-04-29 20:06:22 -0500

    New Doves International Stickers
    24 of 'em.

---
+++ lua/arc9/common/attachments_bulk/dovesinternationalstickers.lua
@@ -0,0 +1,412 @@
+local ATT
+
+ATT = {}
+
+ATT.PrintName = "Doves International Logo (ARC9 Colorway)"
+ATT.CompactName = "ARC9"
+ATT.Description = [[Doves International Logo in the ARC9 color scheme.
+Design by M541/Neosun.]]
+ATT.Icon = Material("stickers/dovesinternational/arc9")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "ARC9/Doves International"
+ATT.SortOrder = 9999
+
+ATT.StickerMaterial = "stickers/dovesinternational/arc9"
+
+ARC9.LoadAttachment(ATT, "sticker_dovesinternational_arc9")
+ATT = {}
+
+ATT.PrintName = "Doves International Logo (Arctic Colorway)"
+ATT.CompactName = "Arctic"
+ATT.Description = [[Doves International Logo in an Arctic White color scheme.
+Design by M541/Neosun.]]
+ATT.Icon = Material("stickers/dovesinternational/arctic")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "ARC9/Doves International"
+ATT.SortOrder = 9999
+
+ATT.StickerMaterial = "stickers/dovesinternational/arctic"
+
+ARC9.LoadAttachment(ATT, "sticker_dovesinternational_arctic")
+ATT = {}
+
+ATT.PrintName = "Doves International Logo (Black Colorway)"
+ATT.CompactName = "Black"
+ATT.Description = [[Doves International Logo in a Black color scheme.
+Design by M541/Neosun.]]
+ATT.Icon = Material("stickers/dovesinternational/black")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "ARC9/Doves International"
+ATT.SortOrder = 9999
+
+ATT.StickerMaterial = "stickers/dovesinternational/black"
+
+ARC9.LoadAttachment(ATT, "sticker_dovesinternational_black")
+ATT = {}
+
+ATT.PrintName = "Doves International Logo (Black & Blue Colorway)"
+ATT.CompactName = "Black & Blue"
+ATT.Description = [[Doves International Logo in a Black & Blue color scheme.
+Design by M541/Neosun.]]
+ATT.Icon = Material("stickers/dovesinternational/blackblue")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "ARC9/Doves International"
+ATT.SortOrder = 9999
+
+ATT.StickerMaterial = "stickers/dovesinternational/blackblue"
+
+ARC9.LoadAttachment(ATT, "sticker_dovesinternational_blackblue")
+ATT = {}
+
+ATT.PrintName = "Doves International Logo (Black & Green Colorway)"
+ATT.CompactName = "Black & Green"
+ATT.Description = [[Doves International Logo in a Black & Green color scheme.
+Design by M541/Neosun.]]
+ATT.Icon = Material("stickers/dovesinternational/blackgreen")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "ARC9/Doves International"
+ATT.SortOrder = 9999
+
+ATT.StickerMaterial = "stickers/dovesinternational/blackgreen"
+
+ARC9.LoadAttachment(ATT, "sticker_dovesinternational_blackgreen")
+ATT = {}
+
+ATT.PrintName = "Doves International Logo (Black & Tan Colorway)"
+ATT.CompactName = "Black & Tan"
+ATT.Description = [[Doves International Logo in a Black & Tan color scheme.
+Design by M541/Neosun.]]
+ATT.Icon = Material("stickers/dovesinternational/blacktan")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "ARC9/Doves International"
+ATT.SortOrder = 9999
+
+ATT.StickerMaterial = "stickers/dovesinternational/blacktan"
+
+ARC9.LoadAttachment(ATT, "sticker_dovesinternational_blacktan")
+ATT = {}
+
+ATT.PrintName = "Doves International Logo (Transparent, Black)"
+ATT.CompactName = "Transparent (Black)"
+ATT.Description = [[Doves International Logo, Transparent Monochromatic (Black Version).
+Design by M541/Neosun.]]
+ATT.Icon = Material("stickers/dovesinternational/blacktrans")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "ARC9/Doves International"
+ATT.SortOrder = 2
+
+ATT.StickerMaterial = "stickers/dovesinternational/blacktrans"
+
+ARC9.LoadAttachment(ATT, "sticker_dovesinternational_blacktrans")
+ATT = {}
+
+ATT.PrintName = "Doves International Logo (Blue Colorway)"
+ATT.CompactName = "Blue"
+ATT.Description = [[Doves International Logo in a Blue color scheme.
+Design by M541/Neosun.]]
+ATT.Icon = Material("stickers/dovesinternational/blue")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "ARC9/Doves International"
+ATT.SortOrder = 9999
+
+ATT.StickerMaterial = "stickers/dovesinternational/blue"
+
+ARC9.LoadAttachment(ATT, "sticker_dovesinternational_blue")
+ATT = {}
+
+ATT.PrintName = "Doves International Logo (Bronze Colorway)"
+ATT.CompactName = "Bronze"
+ATT.Description = [[Doves International Logo in a Bronze color scheme.
+Design by M541/Neosun.]]
+ATT.Icon = Material("stickers/dovesinternational/bronze")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "ARC9/Doves International"
+ATT.SortOrder = 9999
+
+ATT.StickerMaterial = "stickers/dovesinternational/bronze"
+
+ARC9.LoadAttachment(ATT, "sticker_dovesinternational_bronze")
+ATT = {}
+
+ATT.PrintName = "Doves International Logo"
+ATT.CompactName = "Doves International"
+ATT.Description = [[Doves International Logo in its standard color configuration.
+Design by M541/Neosun.]]
+ATT.Icon = Material("stickers/dovesinternational/dovesinternational")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "ARC9/Doves International"
+ATT.SortOrder = 1
+
+ATT.StickerMaterial = "stickers/dovesinternational/dovesinternational"
+
+ARC9.LoadAttachment(ATT, "sticker_dovesinternational_dovesinternational")
+ATT = {}
+
+ATT.PrintName = "Doves International Logo (Error Colorway)"
+ATT.CompactName = "ERROR"
+ATT.Description = [[Doves International Logo in a Missing Textures color scheme.
+Design by M541/Neosun.]]
+ATT.Icon = Material("stickers/dovesinternational/error")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "ARC9/Doves International"
+ATT.SortOrder = 9999
+
+ATT.StickerMaterial = "stickers/dovesinternational/error"
+
+ARC9.LoadAttachment(ATT, "sticker_dovesinternational_error")
+ATT = {}
+
+ATT.PrintName = "Doves International Logo (FOXHOUND Colorway)"
+ATT.CompactName = "FOXHOUND"
+ATT.Description = [[Doves International Logo, colored to resemble a certain special forces unit.
+Design by M541/Neosun.]]
+ATT.Icon = Material("stickers/dovesinternational/fox")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "ARC9/Doves International"
+ATT.SortOrder = 9999
+
+ATT.StickerMaterial = "stickers/dovesinternational/fox"
+
+ARC9.LoadAttachment(ATT, "sticker_dovesinternational_fox")
+ATT = {}
+
+ATT.PrintName = "Doves International Logo (Full Color)"
+ATT.CompactName = "Full Color"
+ATT.Description = [[Doves International Logo in full color.
+Design by M541/Neosun.]]
+ATT.Icon = Material("stickers/dovesinternational/fullcolor")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "ARC9/Doves International"
+ATT.SortOrder = 9999
+
+ATT.StickerMaterial = "stickers/dovesinternational/fullcolor"
+
+ARC9.LoadAttachment(ATT, "sticker_dovesinternational_fullcolor")
+ATT = {}
+
+ATT.PrintName = "Doves International Logo (Green Colorway)"
+ATT.CompactName = "Green"
+ATT.Description = [[Doves International Logo in a Green color scheme.
+Design by M541/Neosun.]]
+ATT.Icon = Material("stickers/dovesinternational/green")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "ARC9/Doves International"
+ATT.SortOrder = 9999
+
+ATT.StickerMaterial = "stickers/dovesinternational/green"
+
+ARC9.LoadAttachment(ATT, "sticker_dovesinternational_green")
+ATT = {}
+
+ATT.PrintName = "Doves International Logo (Grey Colorway)"
+ATT.CompactName = "Grey"
+ATT.Description = [[Doves International Logo in a Grey color scheme.
+Design by M541/Neosun.]]
+ATT.Icon = Material("stickers/dovesinternational/grey")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "ARC9/Doves International"
+ATT.SortOrder = 9999
+
+ATT.StickerMaterial = "stickers/dovesinternational/grey"
+
+ARC9.LoadAttachment(ATT, "sticker_dovesinternational_grey")
+ATT = {}
+
+ATT.PrintName = "Doves International Logo (Monochrome Colorway)"
+ATT.CompactName = "Monochrome"
+ATT.Description = [[Doves International Logo in a Black & White color scheme.
+Design by M541/Neosun.]]
+ATT.Icon = Material("stickers/dovesinternational/monochrome")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "ARC9/Doves International"
+ATT.SortOrder = 2
+
+ATT.StickerMaterial = "stickers/dovesinternational/monochrome"
+
+ARC9.LoadAttachment(ATT, "sticker_dovesinternational_monochrome")
+ATT = {}
+
+ATT.PrintName = "Doves International Logo (Navy & Tan Colorway)"
+ATT.CompactName = "Navy & Tan"
+ATT.Description = [[Doves International Logo in a Navy & Tan color scheme.
+Design by M541/Neosun.]]
+ATT.Icon = Material("stickers/dovesinternational/navytan")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "ARC9/Doves International"
+ATT.SortOrder = 9999
+
+ATT.StickerMaterial = "stickers/dovesinternational/navytan"
+
+ARC9.LoadAttachment(ATT, "sticker_dovesinternational_navytan")
+ATT = {}
+
+ATT.PrintName = "Doves International Logo (Pink Colorway)"
+ATT.CompactName = "Pink"
+ATT.Description = [[Doves International Logo in a Pink color scheme.
+Design by M541/Neosun.]]
+ATT.Icon = Material("stickers/dovesinternational/pink")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "ARC9/Doves International"
+ATT.SortOrder = 9999
+
+ATT.StickerMaterial = "stickers/dovesinternational/pink"
+
+ARC9.LoadAttachment(ATT, "sticker_dovesinternational_pink")
+ATT = {}
+
+ATT.PrintName = "Doves International Logo (Purple Colorway)"
+ATT.CompactName = "Purple"
+ATT.Description = [[Doves International Logo in a Purple color scheme.
+Design by M541/Neosun.]]
+ATT.Icon = Material("stickers/dovesinternational/purple")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "ARC9/Doves International"
+ATT.SortOrder = 9999
+
+ATT.StickerMaterial = "stickers/dovesinternational/purple"
+
+ARC9.LoadAttachment(ATT, "sticker_dovesinternational_purple")
+ATT = {}
+
+ATT.PrintName = "Doves International Logo (Tan Colorway)"
+ATT.CompactName = "Tan"
+ATT.Description = [[Doves International Logo in a Tan color scheme.
+Design by M541/Neosun.]]
+ATT.Icon = Material("stickers/dovesinternational/tan")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "ARC9/Doves International"
+ATT.SortOrder = 9999
+
+ATT.StickerMaterial = "stickers/dovesinternational/tan"
+
+ARC9.LoadAttachment(ATT, "sticker_dovesinternational_tan")
+ATT = {}
+
+ATT.PrintName = "Doves International Logo (Teal & Purple Colorway)"
+ATT.CompactName = "Teal"
+ATT.Description = [[Doves International Logo in a Teal & Purple color scheme.
+Design by M541/Neosun.]]
+ATT.Icon = Material("stickers/dovesinternational/teal")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "ARC9/Doves International"
+ATT.SortOrder = 9999
+
+ATT.StickerMaterial = "stickers/dovesinternational/teal"
+
+ARC9.LoadAttachment(ATT, "sticker_dovesinternational_teal")
+ATT = {}
+
+ATT.PrintName = "Doves International Logo (Trans Pride Colorway)"
+ATT.CompactName = "Transgender"
+ATT.Description = [[Doves International Logo, colored to resemble the Transgender Pride Flag.
+
+Fun Fact: Dorito, the official mascot of Doves International, is Transfeminine and goes by They/Them pronouns.
+Design by M541/Neosun.]]
+ATT.Icon = Material("stickers/dovesinternational/trans")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "ARC9/Doves International"
+ATT.SortOrder = 9999
+
+ATT.StickerMaterial = "stickers/dovesinternational/trans"
+
+ARC9.LoadAttachment(ATT, "sticker_dovesinternational_trans")
+ATT = {}
+
+ATT.PrintName = "Doves International Logo (White Colorway)"
+ATT.CompactName = "White"
+ATT.Description = [[Doves International Logo in a White color scheme.
+Design by M541/Neosun.]]
+ATT.Icon = Material("stickers/dovesinternational/white")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "ARC9/Doves International"
+ATT.SortOrder = 9999
+
+ATT.StickerMaterial = "stickers/dovesinternational/white"
+
+ARC9.LoadAttachment(ATT, "sticker_dovesinternational_white")
+ATT = {}
+
+ATT.PrintName = "Doves International Logo (Transparent, White)"
+ATT.CompactName = "Transparent (White)"
+ATT.Description = [[Doves International Logo, Transparent Monochromatic (White Version)
+Design by M541/Neosun.]]
+ATT.Icon = Material("stickers/dovesinternational/whitetrans")
+
+ATT.Free = true
+
+ATT.Category = "stickers"
+ATT.Folder = "ARC9/Doves International"
+ATT.SortOrder = 2
+
+ATT.StickerMaterial = "stickers/dovesinternational/whitetrans"
+
+ARC9.LoadAttachment(ATT, "sticker_dovesinternational_whitetrans")
