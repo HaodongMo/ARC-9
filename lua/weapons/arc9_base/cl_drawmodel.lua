7527bc4465000d61c96589bcea09b129a9f943ae
Darsu <95997959+dar-su@users.noreply.github.com>
2025-11-23 02:01:00 +0700

    revert renderorigin change, this made it worse

--- lua/weapons/arc9_base/cl_drawmodel.lua
+++ lua/weapons/arc9_base/cl_drawmodel.lua
@@ -179,7 +179,7 @@ function SWEP:DrawCustomModel(wm, custompos, customang)
 
 
             if !model.NoDraw and !(model.istranslucent and !ARC9.PresetCam and !onground and !isnpc) then
-                if !wm then model:SetRenderOrigin(self.ViewModelPos or (IsValid(self:GetVM()) and self:GetVM():GetPos() or self:GetPos())) end
+                -- if !wm then model:SetRenderOrigin(self.ViewModelPos or (IsValid(self:GetVM()) and self:GetVM():GetPos() or self:GetPos())) end
                 model:DrawModel()
                 if drawprojlights:GetBool() or rttenabled == false then render.RenderFlashlights(function() model:DrawModel() end) end
             end
